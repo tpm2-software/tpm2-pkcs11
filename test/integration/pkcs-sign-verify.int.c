feb787c6d3ebd1fb8458ddb1b774308102ebb50f
William Roberts <william.c.roberts@intel.com>
2018-09-25 14:01:01 -0700

    LICENSE: correct license
    The SPDC identifier wasn't correct. Update it to correctly
    reflect the actual BSD2 license.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/integration/pkcs-sign-verify.int.c
+++ test/integration/pkcs-sign-verify.int.c
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: Apache-2.0 */
+/* SPDX-License-Identifier: BSD-2 */
 /***********************************************************************
  * Copyright (c) 2018, Intel Corporation
  *
