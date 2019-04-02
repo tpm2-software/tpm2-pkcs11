8ec916f2a866fcbd2fb665632b1a53c21c035130
Markus Linnala <markus.linnala@gmail.com>
2019-04-02 19:37:25 +0300

    tools/setup.py: Add long_description
    Signed-off-by: Markus Linnala <markus.linnala@gmail.com>

--- tools/setup.py
+++ tools/setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+readme = path.join(path.dirname(__file__), '..', 'docs', 'README.md')
+with open(readme, encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='tpm2-pkcs11-tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version='1.33.7',
     description='Command line tools for the TPM2.0 PKCS11 module',
     url='https://github.com/tpm2-software/tpm2-pkcs11',
@@ -26,5 +34,6 @@ setup(
     ],
     tests_require=[],
     entry_points={
-        'console_scripts': ['tpm2_ptool = tpm2_pkcs11.tpm2_ptool:main',],
-    },)
+        'console_scripts': ['tpm2_ptool = tpm2_pkcs11.tpm2_ptool:main'],
+    },
+)
