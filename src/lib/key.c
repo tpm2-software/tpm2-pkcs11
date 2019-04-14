tpm2-software/tpm2-pkcs11

commit 9f043bf8f9bb776b3f3be78f8b54f11458ab464e

object: make public objects a first class citizen
The linking mechanism made C_DestroyObject and certificate support
a nightmare.

Just make the public portion of an object a seperate and descrete object
in the db that uses only the public portion of a TPM object and can
be loaded to the null hierarchy via loadexternal.

Signed-off-by: William Roberts <william.c.roberts@intel.com>

src/lib/key.c
@@ -94,7 +94,8 @@ CK_RV object_add_missing_mechs(tobject *tobj, CK_MECHANISM_TYPE mech) {
     }
 }
 
-static CK_RV ecc_add_missing_attrs(tobject *tobj, tpm_object_data *objdata) {
+static CK_RV ecc_add_missing_attrs(tobject *public_tobj, tobject *private_tobj, tpm_object_data *objdata) {
+    UNUSED(private_tobj);
 
     CK_RV tmp_rv;
     CK_RV rv = CKR_HOST_MEMORY;
@@ -102,13 +103,13 @@ static CK_RV ecc_add_missing_attrs(tobject *tobj, tpm_object_data *objdata) {
     CK_ULONG pubindex = 0;
     CK_ATTRIBUTE newpubattrs[1] = { 0 };
 
-    CK_ATTRIBUTE_PTR a = object_get_pub_attr_by_type(tobj, CKA_EC_POINT);
+    CK_ATTRIBUTE_PTR a = object_get_attribute_by_type(public_tobj, CKA_EC_POINT);
     if (!a) {
         ADD_ATTR_TWIST(CKA_EC_POINT, objdata->ecc.ecpoint, newpubattrs, pubindex);
     }
 
     /* add the new attrs */
-    rv = tobject_append_attrs(tobj, true, newpubattrs, pubindex);
+    rv = tobject_append_attrs(public_tobj, newpubattrs, pubindex);
 
 error:
 
@@ -121,7 +122,7 @@ error:
     return rv;
 }
 
-static CK_RV rsa_add_missing_attrs(tobject *tobj, tpm_object_data *objdata) {
+static CK_RV rsa_add_missing_attrs(tobject *public_tobj, tobject *private_tobj, tpm_object_data *objdata) {
 
     CK_RV tmp_rv;
     CK_RV rv = CKR_HOST_MEMORY;
@@ -132,12 +133,12 @@ static CK_RV rsa_add_missing_attrs(tobject *tobj, tpm_object_data *objdata) {
     CK_ULONG pubindex = 0;
     CK_ATTRIBUTE newpubattrs[3] = { 0 };
 
-    CK_ATTRIBUTE_PTR a = object_get_pub_attr_by_type(tobj, CKA_MODULUS);
+    CK_ATTRIBUTE_PTR a = object_get_attribute_by_type(public_tobj, CKA_MODULUS);
     if (!a) {
         ADD_ATTR_TWIST(CKA_MODULUS, objdata->rsa.modulus, newpubattrs, pubindex);
     }
 
-    a = object_get_pub_attr_by_type(tobj, CKA_PUBLIC_EXPONENT);
+    a = object_get_attribute_by_type(public_tobj, CKA_PUBLIC_EXPONENT);
     if (!a) {
         BIGNUM *b = BN_new();
         if (!b) {
@@ -177,17 +178,17 @@ static CK_RV rsa_add_missing_attrs(tobject *tobj, tpm_object_data *objdata) {
 
     /* make sure both have keybits specified via CKA_MODULUS_BITS */
     CK_ULONG keybits = twist_len(objdata->rsa.modulus) * 8;
-    a = object_get_priv_attr_by_type(tobj, CKA_MODULUS_BITS);
+    a = object_get_attribute_by_type(private_tobj, CKA_MODULUS_BITS);
     if (!a) {
         ADD_ATTR(CK_ULONG, CKA_MODULUS_BITS, keybits, newprivattrs, privindex);
     }
 
-    a = object_get_pub_attr_by_type(tobj, CKA_MODULUS_BITS);
+    a = object_get_attribute_by_type(public_tobj, CKA_MODULUS_BITS);
     if (!a) {
         ADD_ATTR(CK_ULONG, CKA_MODULUS_BITS, keybits, newpubattrs, pubindex);
     }
 
-    rv = tobject_append_attrs(tobj, true, newpubattrs, pubindex);
+    rv = tobject_append_attrs(public_tobj, newpubattrs, pubindex);
 
 error:
     tmp_rv = utils_attr_free(newprivattrs, privindex);
@@ -205,12 +206,8 @@ error:
     return rv;
 }
 
-static CK_RV object_add_missing_attrs(tobject *tobj, tpm_object_data *objdata, CK_MECHANISM_TYPE mech) {
+static CK_RV object_add_missing_attrs(tobject *public_tobj, tobject *private_tobj, tpm_object_data *objdata, CK_MECHANISM_TYPE mech) {
 
-    /*
-     * this function assumes a partially formed tobject with an id of 0, which
-     * means underlying calls will always use private.
-     */
     CK_KEY_TYPE keytype;
     switch (mech) {
     case CKM_RSA_PKCS_KEY_PAIR_GEN:
@@ -232,12 +229,15 @@ static CK_RV object_add_missing_attrs(tobject *tobj, tpm_object_data *objdata, C
     CK_ATTRIBUTE newprivattrs[8] = { 0 };
     CK_ATTRIBUTE newpubattrs[8] = { 0 };
 
-    CK_ATTRIBUTE_PTR a = object_get_pub_attr_by_type(tobj, CKA_KEY_TYPE);
+    /*
+     * Ensure that keytype is set for both public and private
+     */
+    CK_ATTRIBUTE_PTR a = object_get_attribute_by_type(public_tobj, CKA_KEY_TYPE);
     if (!a) {
         ADD_ATTR(CK_KEY_TYPE, CKA_KEY_TYPE, keytype, newpubattrs, pubindex);
     }
 
-    a = object_get_priv_attr_by_type(tobj, CKA_KEY_TYPE);
+    a = object_get_attribute_by_type(private_tobj, CKA_KEY_TYPE);
     if (!a) {
         ADD_ATTR(CK_KEY_TYPE, CKA_KEY_TYPE, keytype, newprivattrs, privindex);
     }
@@ -249,15 +249,15 @@ static CK_RV object_add_missing_attrs(tobject *tobj, tpm_object_data *objdata, C
        .pValue = &class
     };
 
-    a = object_get_priv_attr_full(tobj, &match);
+    a = object_get_attribute_full(private_tobj, &match);
     if (!a) {
         ADD_ATTR(CK_OBJECT_CLASS, CKA_CLASS, CKO_PRIVATE_KEY, newprivattrs, privindex);
     }
 
     class = CKO_PUBLIC_KEY;
-    a = object_get_pub_attr_full(tobj, &match);
+    a = object_get_attribute_full(public_tobj, &match);
     if (!a) {
-        ADD_ATTR(CK_OBJECT_CLASS, CKA_CLASS, CKO_PUBLIC_KEY,  newpubattrs, pubindex);
+        ADD_ATTR(CK_OBJECT_CLASS, CKA_CLASS, CKO_PUBLIC_KEY, newpubattrs, pubindex);
     }
 
     /*
@@ -273,7 +273,7 @@ static CK_RV object_add_missing_attrs(tobject *tobj, tpm_object_data *objdata, C
      */
 
     CK_BBOOL sensitive = CK_TRUE;
-    a = object_get_attribute_by_type(tobj, CKA_SENSITIVE);
+    a = object_get_attribute_by_type(private_tobj, CKA_SENSITIVE);
     if (!a) {
         ADD_ATTR(CK_BBOOL, CKA_SENSITIVE, CK_TRUE, newprivattrs, privindex);
     } else {
@@ -284,14 +284,14 @@ static CK_RV object_add_missing_attrs(tobject *tobj, tpm_object_data *objdata, C
     }
 
     /* mark always sensitive if not specified by user */
-    a = object_get_attribute_by_type(tobj, CKA_ALWAYS_SENSITIVE);
+    a = object_get_attribute_by_type(private_tobj, CKA_ALWAYS_SENSITIVE);
     if (!a) {
         ADD_ATTR(CK_BBOOL, CKA_ALWAYS_SENSITIVE, sensitive ? CK_TRUE : CK_FALSE, newprivattrs, privindex);
     }
 
     /* if the object is missing CKA_EXTRACTABLE use the value of CKA_SENSITIVE to determine */
     CK_BBOOL extractable = CK_FALSE;
-    a = object_get_attribute_by_type(tobj, CKA_EXTRACTABLE);
+    a = object_get_attribute_by_type(private_tobj, CKA_EXTRACTABLE);
     if (!a) {
         ADD_ATTR(CK_BBOOL, CKA_EXTRACTABLE, !sensitive, newprivattrs, privindex);
     } else {
@@ -302,7 +302,7 @@ static CK_RV object_add_missing_attrs(tobject *tobj, tpm_object_data *objdata, C
     }
 
     /* mark never extractable if not specified by user */
-    a = object_get_attribute_by_type(tobj, CKA_NEVER_EXTRACTABLE);
+    a = object_get_attribute_by_type(private_tobj, CKA_NEVER_EXTRACTABLE);
     if (!a) {
         ADD_ATTR(CK_BBOOL, CKA_NEVER_EXTRACTABLE, extractable ? CK_FALSE : CK_TRUE, newprivattrs, privindex);
     }
@@ -311,10 +311,10 @@ static CK_RV object_add_missing_attrs(tobject *tobj, tpm_object_data *objdata, C
     /* TODO dispatch table */
     switch (keytype) {
     case CKK_RSA:
-        rv = rsa_add_missing_attrs(tobj, objdata);
+        rv = rsa_add_missing_attrs(public_tobj, private_tobj, objdata);
         break;
     case CKK_EC:
-        rv = ecc_add_missing_attrs(tobj, objdata);
+        rv = ecc_add_missing_attrs(public_tobj, private_tobj, objdata);
         break;
     default:
         rv = CKR_GENERAL_ERROR;
@@ -328,12 +328,12 @@ static CK_RV object_add_missing_attrs(tobject *tobj, tpm_object_data *objdata, C
     }
 
     /* add the new attrs */
-    rv = tobject_append_attrs(tobj, false, newprivattrs, privindex);
+    rv = tobject_append_attrs(private_tobj, newprivattrs, privindex);
     if (rv != CKR_OK) {
         goto error;
     }
 
-    rv = tobject_append_attrs(tobj, true, newpubattrs, pubindex);
+    rv = tobject_append_attrs(public_tobj, newpubattrs, pubindex);
 
 error:
     tmp_rv = utils_attr_free(newprivattrs, privindex);
@@ -361,29 +361,30 @@ static CK_RV object_add_missing_ids(tobject *priv_tobj) {
     CK_ATTRIBUTE newprivattrs[1] = { 0 };
     CK_ATTRIBUTE newpubattrs[1] = { 0 };
 
-    CK_ATTRIBUTE_PTR a = object_get_priv_attr_by_type(priv_tobj, CKA_ID);
+    CK_ATTRIBUTE_PTR a = object_get_attribute_by_type(priv_tobj, CKA_ID);
     if (!a) {
         char tmp[32];
         snprintf(tmp, sizeof(tmp), "%lu", priv_tobj->id);
         ADD_ATTR_STR(CKA_ID, tmp, newprivattrs, privindex);
     }
 
-    a = object_get_pub_attr_by_type(priv_tobj, CKA_ID);
+    a = object_get_attribute_by_type(priv_tobj, CKA_ID);
     if (!a) {
-        tobject *pub_tobj = priv_tobj->link;
-        assert(pub_tobj);
-        char tmp[32];
-        snprintf(tmp, sizeof(tmp), "%lu", pub_tobj->id);
-        ADD_ATTR_STR(CKA_ID, tmp, newpubattrs, pubindex);
+        // XXX FIXME
+        //        tobject *pub_tobj = priv_tobj->link;
+//        assert(pub_tobj);
+//        char tmp[32];
+//        snprintf(tmp, sizeof(tmp), "%lu", pub_tobj->id);
+//        ADD_ATTR_STR(CKA_ID, tmp, newpubattrs, pubindex);
     }
 
     /* add the new attrs */
-    rv = tobject_append_attrs(priv_tobj, false, newprivattrs, privindex);
+    rv = tobject_append_attrs(priv_tobj, newprivattrs, privindex);
     if (rv != CKR_OK) {
         goto error;
     }
 
-    rv = tobject_append_attrs(priv_tobj, true, newpubattrs, pubindex);
+    rv = tobject_append_attrs(priv_tobj, newpubattrs, pubindex);
 
 error:
     tmp_rv = utils_attr_free(newprivattrs, privindex);
@@ -532,6 +533,11 @@ CK_RV key_gen (
         goto out;
     }
 
+    new_public_tobj = tobject_new();
+    if (!new_public_tobj) {
+        goto out;
+    }
+
     rv = utils_new_random_object_auth(&newauthbin, &newauthhex);
     if (rv != CKR_OK) {
         LOGE("Failed to create new object auth");
@@ -558,61 +564,93 @@ CK_RV key_gen (
         goto out;
     }
 
-    rv = tobject_append_attrs(new_private_tobj, true, public_key_template, public_key_attribute_count);
+    /* set the tpm object handles */
+    tobject_set_handle(new_private_tobj, objdata.privhandle);
+    tobject_set_handle(new_public_tobj, objdata.pubhandle);
+
+    rv = tobject_append_attrs(new_public_tobj, public_key_template, public_key_attribute_count);
     if (rv != CKR_OK) {
         LOGE("Failed to append public template");
         goto out;
     }
 
-    rv = tobject_append_attrs(new_private_tobj, false, private_key_template, private_key_attribute_count);
+    rv = tobject_append_attrs(new_private_tobj, private_key_template, private_key_attribute_count);
     if (rv != CKR_OK) {
         LOGE("Failed to append private template");
         goto out;
     }
 
     /*
-     * Need to convert the generation to mech to supported object mechs.
+     * objects have default required attributes, add them if not present.
      */
     /* TODO dispatch table here */
-    rv = object_add_missing_attrs(new_private_tobj, &objdata, mechanism->mechanism);
+    rv = object_add_missing_attrs(new_public_tobj, new_private_tobj, &objdata, mechanism->mechanism);
     if (rv != CKR_OK) {
         LOGE("Failed to add missing rsa attrs");
         goto out;
     }
 
-    tobject_set_auth(new_private_tobj, newauthbin, newwrapped_auth);
-    tobject_set_blob_data(new_private_tobj, objdata.pubblob, objdata.privblob);
-    tobject_set_handle(new_private_tobj, objdata.handle);
+    /* populate blob data */
+    rv = tobject_set_blob_data(new_private_tobj, objdata.pubblob, objdata.privblob);
+    if (rv != CKR_OK) {
+        goto out;
+    }
 
-    /* dispatch table here */
+    rv = tobject_set_blob_data(new_public_tobj, objdata.pubblob, NULL);
+    if (rv != CKR_OK) {
+        goto out;
+    }
+
+    /* populate auth data */
+    rv = tobject_set_auth(new_public_tobj, newauthbin, newwrapped_auth);
+    if (rv != CKR_OK) {
+        goto out;
+    }
+
+    rv = tobject_set_auth(new_private_tobj, newauthbin, newwrapped_auth);
+    if (rv != CKR_OK) {
+        goto out;
+    }
+
+    /*
+     * Add the missing supported mechanisms
+     */
     rv = object_add_missing_mechs(new_private_tobj, mechanism->mechanism);
     if (rv != CKR_OK) {
-        LOGE("Failed to add missing rsa mechanisms");
+        LOGE("Failed to add missing mechanisms");
+        goto out;
+    }
+
+    rv = object_add_missing_mechs(new_public_tobj, mechanism->mechanism);
+    if (rv != CKR_OK) {
+        LOGE("Failed to add missing mechanisms");
         goto out;
     }
 
     /* populates tobj->id */
     rv = db_add_new_object(tok, new_private_tobj);
     if (rv != CKR_OK) {
-        LOGE("Failed to add object to db");
+        LOGE("Failed to add private object to db");
         goto out;
     }
 
-    new_public_tobj = tobject_link(new_private_tobj);
-    if (!new_public_tobj) {
+    rv = db_add_new_object(tok, new_public_tobj);
+    if (rv != CKR_OK) {
+        LOGE("Failed to add public object to db");
         goto out;
     }
 
-    /* set CKA_ID if not present */
+    /* set CKA_ID if not present based on tobj->id */
     rv = object_add_missing_ids(new_private_tobj);
     if (rv != CKR_OK) {
         LOGE("Failed to add missing CKA_ID's");
         goto out;
     }
 
-    rv = object_add_missing_mechs(new_private_tobj, mechanism->mechanism);
+    /* set CKA_ID if not present based on tobj->id */
+    rv = object_add_missing_ids(new_public_tobj);
     if (rv != CKR_OK) {
-        LOGE("Failed to add missing mechanisms");
+        LOGE("Failed to add missing CKA_ID's");
         goto out;
     }
 
@@ -633,6 +671,8 @@ out:
 
     tpm_objdata_free(&objdata);
     twist_free(newauthhex);
+    twist_free(newauthbin);
+    twist_free(newwrapped_auth);
 
     if (rv != CKR_OK) {
         tobject_free(new_private_tobj);

src/lib/db.c
@@ -93,11 +93,15 @@ static int get_token_count(size_t *cnt) {
     return sqlite3_exec(global.db, sql, token_count_cb, cnt, NULL);
 }
 
-static int get_blob(sqlite3_stmt *stmt, int i, twist *blob) {
+static int _get_blob(sqlite3_stmt *stmt, int i, bool can_be_null, twist *blob) {
 
     int size = sqlite3_column_bytes(stmt, i);
-    if (size <= 0) {
+    if (size < 0) {
         return 1;
+    }
+
+    if (size == 0) {
+        return can_be_null ? 0 : 1;
     }
 
     const void *data = sqlite3_column_blob(stmt, i);
@@ -108,6 +112,16 @@ static int get_blob(sqlite3_stmt *stmt, int i, twist *blob) {
     }
 
     return 0;
+}
+
+static int get_blob_null(sqlite3_stmt *stmt, int i, twist *blob) {
+
+    return _get_blob(stmt, i, true, blob);
+}
+
+static int get_blob(sqlite3_stmt *stmt, int i, twist *blob) {
+
+    return _get_blob(stmt, i, false, blob);
 }
 
 typedef struct token_get_cb_ud token_get_cb_ud;
@@ -582,16 +596,11 @@ tobject *db_tobject_new(sqlite3_stmt *stmt) {
 
         if (!strcmp(name, "id")) {
             tobj->id = sqlite3_column_int(stmt, i);
-            bool range_ok = tobject_id_range_ok(tobj->id);
-            if (!range_ok) {
-                LOGE("tobj->id out of range");
-                goto error;
-            }
 
         } else if (!strcmp(name, "sid")) {
             // Ignore sid we don't need it as sobject has that data.
         } else if (!strcmp(name, "priv")) {
-            goto_error(get_blob(stmt, i, &tobj->priv), error);
+            goto_error(get_blob_null(stmt, i, &tobj->priv), error);
 
         } else if (!strcmp(name, "pub")) {
             goto_error(get_blob(stmt, i, &tobj->pub), error);
@@ -599,25 +608,15 @@ tobject *db_tobject_new(sqlite3_stmt *stmt) {
         } else if (!strcmp(name, "objauth")) {
             tobj->objauth = twist_new((char *)sqlite3_column_text(stmt, i));
             goto_oom(tobj->objauth, error);
-        } else if (!strcmp(name, "pubattrs")
-                || !strcmp(name, "privattrs")) {
-            bool is_pub = !strcmp(name, "pubattrs");
-            objattrs *tobj_attrs = is_pub ? &tobj->atributes.pub : &tobj->atributes.priv;
-            const char *attrs = (const char *)sqlite3_column_text(stmt, i);
-            /* These can be null for keys without public portions, like AES keys */
-            if (!attrs) {
-                /* skip public tobject attrs when NULL, for things like AES */
-                if (is_pub) {
-                    continue;
-                }
+        } else if (!strcmp(name, "attrs")) {
 
-                LOGE("No private attributes for object in DB");
+            const char *attrs = (const char *)sqlite3_column_text(stmt, i);
+            if (!attrs) {
+                LOGE("tobject does not have attributes");
                 goto error;
             }
 
-            assert(attrs);
-
-            CK_RV rv = parse_generic_kvp_line(attrs, tobj_attrs, alloc_attrs, parse_attrs);
+            CK_RV rv = parse_generic_kvp_line(attrs, &tobj->attrs, alloc_attrs, parse_attrs);
             if (rv != CKR_OK) {
                 if (rv == CKR_HOST_MEMORY) {
                     goto_oom(NULL, error);
@@ -671,42 +670,21 @@ int init_tobjects(unsigned sid, tobject **head) {
     list *cur = NULL;
     while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
 
-        tobject *base = db_tobject_new(stmt);
-        if (!base) {
+        tobject *insert = db_tobject_new(stmt);
+        if (!insert) {
+            LOGE("Failed to initialize tobject from db");
             goto error;
         }
 
-        /*
-         * If an attribute has a public portion (ie is not symmetric) we link it
-         * back to it's tobject that contains both and set it's public bit in the
-         * id, which is the keyhandle to go back.
-         */
-        tobject *link = NULL;
-        bool has_pub = base->atributes.pub.count > 0;
-        if (has_pub) {
-            link = tobject_link(base);
-            if (!link) {
-                tobject_free(base);
-                goto error;
-            }
+        if (!*head) {
+            *head = insert;
+            cur = &insert->l;
+            continue;
         }
 
-        unsigned i;
-        unsigned stop = has_pub ? 2 : 1;
-        for (i=0; i < stop; i++) {
-            tobject *insert = i == 0 ? base : link;
-            assert(insert);
-
-            if (!*head) {
-                *head = insert;
-                cur = &insert->l;
-                continue;
-            }
-
-            assert(cur);
-            cur->next = &insert->l;
-            cur = cur->next;
-        }
+        assert(cur);
+        cur->next = &insert->l;
+        cur = cur->next;
     }
 
     rc = SQLITE_OK;
@@ -1553,11 +1531,8 @@ CK_RV db_add_new_object(token *tok, tobject *tobj) {
     CK_RV rv = CKR_GENERAL_ERROR;
 
     twist m = NULL;
-    twist pubattrs = NULL;
-    twist privattrs = NULL;
+    twist attrs = NULL;
     sqlite3_stmt *stmt = NULL;
-
-    bool has_pub_attrs = tobj->atributes.pub.count > 0;
 
     m = mech_to_kvp(tobj->mechanisms.mech, tobj->mechanisms.count);
     if (!m) {
@@ -1565,49 +1540,23 @@ CK_RV db_add_new_object(token *tok, tobject *tobj) {
         goto error;
     }
 
-    if (has_pub_attrs) {
-        pubattrs = attr_to_kvp(tobj->atributes.pub.attrs, tobj->atributes.pub.count);
-        if (!pubattrs) {
-            LOGE("Could not retrive public attrs");
-            goto error;
-        }
-    }
-
-    privattrs = attr_to_kvp(tobj->atributes.priv.attrs, tobj->atributes.priv.count);
-    if (!privattrs) {
+    attrs = attr_to_kvp(tobj->attrs.attrs, tobj->attrs.count);
+    if (!attrs) {
         LOGE("Could not retrive private attrs");
         goto error;
     }
 
-    const char *sql;
-    if (has_pub_attrs) {
-        static const char *_s =
+    const char *sql =
           "INSERT INTO tobjects ("
             "sid, "       // index: 1 type: INT
             "pub, "       // index: 2 type: BLOB
             "priv, "      // index: 3 type: BLOB
             "objauth, "   // index: 4 type: TEXT
             "mech,"       // index: 5 type: TEXT
-            "privattrs, " // index: 6 type: TEXT
-            "pubattrs "   // index: 7 type: TEXT
-          ") VALUES ("
-            "?,?,?,?,?,?,?"
-          ");";
-        sql = _s;
-    } else {
-        static const char *_s =
-          "INSERT INTO tobjects ("
-            "sid, "       // index: 1 type: INT
-            "pub, "       // index: 2 type: BLOB
-            "priv, "      // index: 3 type: BLOB
-            "objauth, "   // index: 4 type: TEXT
-            "mech,"       // index: 5 type: TEXT
-            "privattrs, " // index: 6 type: TEXT
+            "attrs"       // index: 6 type: TEXT
           ") VALUES ("
             "?,?,?,?,?,?"
           ");";
-        sql = _s;
-    }
 
     int rc = sqlite3_prepare_v2(global.db, sql, -1, &stmt, NULL);
     if (rc != SQLITE_OK) {
@@ -1623,10 +1572,12 @@ CK_RV db_add_new_object(token *tok, tobject *tobj) {
     rc = sqlite3_bind_int(stmt, 1, tok->sobject.id);
     gotobinderror(rc, "sid");
 
-    rc = sqlite3_bind_blob(stmt, 2, tobj->pub, twist_len(tobj->pub), SQLITE_STATIC);
+    rc = sqlite3_bind_blob(stmt, 2, tobj->pub,
+            tobj->pub ? twist_len(tobj->pub) : 0, SQLITE_STATIC);
     gotobinderror(rc, "pub");
 
-    rc = sqlite3_bind_blob(stmt, 3, tobj->priv, twist_len(tobj->priv), SQLITE_STATIC);
+    rc = sqlite3_bind_blob(stmt, 3, tobj->priv,
+            tobj->priv ? twist_len(tobj->priv) : 0, SQLITE_STATIC);
     gotobinderror(rc, "priv");
 
     rc = sqlite3_bind_text(stmt, 4, tobj->objauth, -1, SQLITE_STATIC);
@@ -1635,13 +1586,8 @@ CK_RV db_add_new_object(token *tok, tobject *tobj) {
     rc = sqlite3_bind_text(stmt, 5, m, -1, SQLITE_STATIC);
     gotobinderror(rc, "mech");
 
-    rc = sqlite3_bind_text(stmt, 6, privattrs, -1, SQLITE_STATIC);
-    gotobinderror(rc, "privattrs");
-
-    if (has_pub_attrs) {
-        rc = sqlite3_bind_text(stmt, 6, pubattrs, -1, SQLITE_STATIC);
-        gotobinderror(rc, "pubattrs");
-    }
+    rc = sqlite3_bind_text(stmt, 6, attrs, -1, SQLITE_STATIC);
+    gotobinderror(rc, "attrs");
 
     rc = sqlite3_step(stmt);
     if (rc != SQLITE_DONE) {
@@ -1671,8 +1617,7 @@ CK_RV db_add_new_object(token *tok, tobject *tobj) {
     rv = CKR_OK;
 
 out:
-    twist_free(privattrs);
-    twist_free(pubattrs);
+    twist_free(attrs);
     twist_free(m);
     return rv;
 

src/lib/object.c
@@ -34,14 +34,12 @@ void tobject_free(tobject *tobj) {
     twist_free(tobj->objauth);
     twist_free(tobj->unsealed_auth);
 
-    CK_ULONG i = 0;
-    for (i=0 ; i < 2; i++) {
-        objattrs *a = i == 0 ? &tobj->atributes.pub : &tobj->atributes.priv;
-        CK_RV rv = utils_attr_free(a->attrs, a->count);
-        assert(rv == CKR_OK);
-        free(a->attrs);
-    }
+    objattrs *a = tobject_get_attrs(tobj);
+    CK_RV rv = utils_attr_free(a->attrs, a->count);
+    assert(rv == CKR_OK);
+    free(a->attrs);
 
+    CK_ULONG i = 0;
     for (i=0; i < tobj->mechanisms.count; i++) {
         CK_MECHANISM_PTR m = &tobj->mechanisms.mech[i];
         if (m->pParameter) {
@@ -392,39 +390,11 @@ CK_ATTRIBUTE_PTR object_get_attribute_by_type(tobject *tobj, CK_ATTRIBUTE_TYPE a
     return NULL;
 }
 
-static CK_ATTRIBUTE_PTR _object_get_attr_by_type(objattrs *attrs, CK_ATTRIBUTE_TYPE atype) {
+CK_ATTRIBUTE_PTR object_get_attribute_full(tobject *tobj, CK_ATTRIBUTE_PTR attr) {
+
+    objattrs *attrs = tobject_get_attrs(tobj);
 
     CK_ULONG i;
-
-    for (i=0; i < attrs->count; i++) {
-
-        CK_ATTRIBUTE_PTR a = &attrs->attrs[i];
-
-        if (a->type == atype) {
-            return a;
-        }
-    }
-
-    return NULL;
-
-}
-
-CK_ATTRIBUTE_PTR object_get_pub_attr_by_type(tobject *tobj, CK_ATTRIBUTE_TYPE atype) {
-
-    objattrs *attrs = &tobj->atributes.pub;
-    return _object_get_attr_by_type(attrs, atype);
-}
-
-CK_ATTRIBUTE_PTR object_get_priv_attr_by_type(tobject *tobj, CK_ATTRIBUTE_TYPE atype) {
-
-    objattrs *attrs = &tobj->atributes.priv;
-    return _object_get_attr_by_type(attrs, atype);
-}
-
-CK_ATTRIBUTE_PTR _object_get_attribute_full(objattrs *attrs, CK_ATTRIBUTE_PTR attr) {
-
-    CK_ULONG i;
-
     for (i=0; i < attrs->count; i++) {
 
         CK_ATTRIBUTE_PTR a = &attrs->attrs[i];
@@ -442,24 +412,6 @@ CK_ATTRIBUTE_PTR _object_get_attribute_full(objattrs *attrs, CK_ATTRIBUTE_PTR at
     }
 
     return NULL;
-}
-
-CK_ATTRIBUTE_PTR object_get_attribute_full(tobject *tobj, CK_ATTRIBUTE_PTR attr) {
-
-    objattrs *attrs = tobject_get_attrs(tobj);
-    return _object_get_attribute_full(attrs, attr);
-}
-
-CK_ATTRIBUTE_PTR object_get_pub_attr_full(tobject *tobj, CK_ATTRIBUTE_PTR attr) {
-
-    objattrs *attrs = &tobj->atributes.pub;
-    return _object_get_attribute_full(attrs, attr);
-}
-
-CK_ATTRIBUTE_PTR object_get_priv_attr_full(tobject *tobj, CK_ATTRIBUTE_PTR attr) {
-
-    objattrs *attrs = &tobj->atributes.pub;
-    return _object_get_attribute_full(attrs, attr);
 }
 
 CK_RV object_get_attributes(session_ctx *ctx, CK_OBJECT_HANDLE object, CK_ATTRIBUTE *templ, CK_ULONG count) {
@@ -519,21 +471,44 @@ tobject *tobject_new(void) {
     return tobj;
 }
 
-void tobject_set_blob_data(tobject *tobj, twist pub, twist priv) {
-    assert(priv);
+CK_RV tobject_set_blob_data(tobject *tobj, twist pub, twist priv) {
     assert(pub);
 
-    tobj->priv = priv;
-    tobj->pub = pub;
+    tobj->priv = twist_dup(priv);
+    if (priv && !tobj->priv) {
+        LOGE("oom");
+        return CKR_HOST_MEMORY;
+    }
+
+    tobj->pub = twist_dup(pub);
+    if (!tobj->pub) {
+        twist_free(tobj->priv);
+        LOGE("oom");
+        return CKR_HOST_MEMORY;
+    }
+
+    return CKR_OK;
 }
 
-void tobject_set_auth(tobject *tobj, twist authbin, twist wrappedauthhex) {
+CK_RV tobject_set_auth(tobject *tobj, twist authbin, twist wrappedauthhex) {
     assert(tobj);
     assert(authbin);
     assert(wrappedauthhex);
 
-    tobj->unsealed_auth = authbin;
-    tobj->objauth = wrappedauthhex;
+    tobj->unsealed_auth = twist_dup(authbin);
+    if (!tobj->unsealed_auth) {
+        LOGE("oom");
+        return CKR_HOST_MEMORY;
+    }
+
+    tobj->objauth = twist_dup(wrappedauthhex);
+    if (!tobj->objauth) {
+        LOGE("oom");
+        twist_free(tobj->unsealed_auth);
+        return CKR_HOST_MEMORY;
+    }
+
+    return CKR_OK;
 }
 
 void tobject_set_handle(tobject *tobj, uint32_t handle) {
@@ -542,7 +517,7 @@ void tobject_set_handle(tobject *tobj, uint32_t handle) {
     tobj->handle = handle;
 }
 
-CK_RV tobject_append_attrs(tobject *tobj, bool is_public, CK_ATTRIBUTE_PTR attrs, CK_ULONG count) {
+CK_RV tobject_append_attrs(tobject *tobj, CK_ATTRIBUTE_PTR attrs, CK_ULONG count) {
     assert(tobj);
     assert(attrs);
 
@@ -550,7 +525,7 @@ CK_RV tobject_append_attrs(tobject *tobj, bool is_public, CK_ATTRIBUTE_PTR attrs
         return CKR_OK;
     }
 
-    objattrs *objattrs = is_public ? &tobj->atributes.pub : &tobj->atributes.priv;
+    objattrs *objattrs = tobject_get_attrs(tobj);
 
     size_t offset = objattrs->count;
     size_t newlen = objattrs->count + count;
@@ -596,35 +571,6 @@ CK_RV tobject_append_mechs(tobject *tobj, CK_MECHANISM_PTR mech, CK_ULONG count)
     return utils_mech_deep_copy(mech, count, &tobj->mechanisms.mech[offset]);
 }
 
-static inline CK_OBJECT_HANDLE _tobject_link_mask(void) {
-    const size_t shift = (sizeof(CK_OBJECT_HANDLE) * 8) - 1;
-    const CK_OBJECT_HANDLE mask = 1UL << shift;
-    return mask;
-}
-
-bool tobject_id_range_ok(CK_OBJECT_HANDLE handle) {
-
-    CK_OBJECT_HANDLE mask =_tobject_link_mask();
-    return (handle & mask) == 0;
-}
-
-static inline CK_OBJECT_HANDLE tobject_format_link_id(tobject *linked) {
-
-    CK_OBJECT_HANDLE mask =_tobject_link_mask();
-    return linked->id | mask;
-}
-
-tobject *tobject_link(tobject *linked) {
-
-    tobject *t = tobject_new();
-    if (t) {
-        t->id = tobject_format_link_id(linked);
-        t->link = linked;
-    }
-
-    return t;
-}
-
 objattrs *tobject_get_attrs(tobject *tobj) {
-    return tobj->link ? &tobj->link->atributes.pub : &tobj->atributes.priv;
+    return &tobj->attrs;
 }

commit 96aa2673a833ba462c04f6a7aa31ad7cd8d0d905

key.c: Store private attributes too
With the latest changes the MODULUS_BITS attribute was not stored in the
private tobj. This caused the sign operation to fail, if the key was
generated using the C_GenerateKeyPair Method with the following error
message:

`ERROR on line: "269" in file: "src/lib/sign.c": Signing key has no
modulus`

Fixes: 9f043bf8f9 ("object: make public objects a first class citizen")
Signed-off-by: Peter Huewe <peterhuewe@gmx.de>

src/lib/key.c
@@ -189,6 +189,11 @@ static CK_RV rsa_add_missing_attrs(tobject *public_tobj, tobject *private_tobj, 
     }
 
     rv = tobject_append_attrs(public_tobj, newpubattrs, pubindex);
+    if (rv != CKR_OK) {
+        goto error;
+    }
+
+    rv = tobject_append_attrs(private_tobj, newprivattrs, privindex);
 
 error:
     tmp_rv = utils_attr_free(newprivattrs, privindex);