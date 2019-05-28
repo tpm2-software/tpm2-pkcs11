tpm2-software/tpm2-pkcs11

commit 62a521d4964c8af03d886920be52047b4d60fe44

ecdsa: set a good buffer size on 5.2 returns
CKR_BUFFER_TOO_SMALL and signature == NULL returns ie, section
5.2 returns, are not handled well for ECDSA. This is due
to ECDSA size can vary, and thus generate signature and discard
for size is not a valid estimation. Notably, when you return X
for size and the size can really be > X.

Thus use the keysize and encoding semantics to determining the
keysize on ECDSA lengths.

This is kind of a quick-and-dirty fix-up, however, the ECDSA and
RSA code should be split apart to make things simpler and is
captured in #205.

Fixes: #201

Signed-off-by: William Roberts <william.c.roberts@intel.com>

src/lib/sign.c
@@ -94,6 +94,71 @@ static bool is_mech_supported(CK_MECHANISM_TYPE mech) {
     return false;
 }
 
+static CK_RV ec_fixup_size(CK_MECHANISM_TYPE mech, tobject *tobj, CK_ULONG_PTR signature_len) {
+
+    if (mech != CKM_ECDSA
+            || mech != CKM_ECDSA_SHA1) {
+        /* nothing to fix up */
+        return CKR_OK;
+    }
+
+    CK_ATTRIBUTE_PTR a = object_get_attribute_by_type(tobj, CKA_EC_PARAMS);
+
+    int nid = 0;
+    CK_RV rv = ec_params_to_nid(a, &nid);
+    if (rv != CKR_OK) {
+        return rv;
+    }
+
+    /*
+     * Math below is based off of ECDSA signature:
+     * SEQUENCE (2 elem)
+     *  INTEGER R
+     *  INTEGER S
+     *
+     *  Integers R and S are bounded by keysize in bytes, followed by their
+     *  respective headers(2bytes) followed by the SEQUENCE header(2 bytes)
+     */
+    unsigned keysize;
+
+    switch (nid) {
+    case NID_X9_62_prime192v1:
+        keysize = 24;
+    break;
+    case NID_secp224r1:
+        keysize = 28;
+    break;
+    case NID_X9_62_prime256v1:
+        keysize = 32;
+    break;
+    case NID_secp384r1:
+        keysize = 48;
+    break;
+    case NID_secp521r1:
+        keysize = 66; /* round up */
+    break;
+    default:
+        LOGE("Unsupported nid to tpm signature size maaping: %d", nid);
+        return CKR_CURVE_NOT_SUPPORTED;
+    }
+
+    /* R and S are INTEGER objects with a header and len byte */
+    static const unsigned INT_HDR = 2U;
+    /* R and S are combined in a SEQUENCE object with a header and len byte */
+    static const unsigned SEQ_HDR = 2U;
+    /* an R or S with a high bit set needs an extra nul byte so it's not negative (twos comp)*/
+    static const unsigned EXTRA = 1U;
+
+    unsigned tmp = ((keysize + INT_HDR + EXTRA) * 2); /* x2 1 for R and 1 for S */
+
+    tmp += SEQ_HDR;
+
+    *signature_len = tmp;
+
+    return CKR_OK;
+}
+
+
 static CK_RV common_init(operation op, session_ctx *ctx, CK_MECHANISM_PTR mechanism, CK_OBJECT_HANDLE key) {
 
     check_pointer(mechanism);
@@ -492,6 +557,15 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
 
             digest_op_data_free(&opdata->digest_opdata);
             opdata->digest_opdata = new_digest_state;
+
+            /* ec signature size is not stable between calls, fix it up */
+            tmp = ec_fixup_size(opdata->mtype, opdata->tobj, signature_len);
+            if (tmp != CKR_OK) {
+                digest_op_data_free(&new_digest_state);
+                reset_ctx = false;
+                goto session_out;
+            }
+
         } else if (is_oneshot) {
             twist_free(opdata->buffer);
             opdata->buffer = NULL;

src/lib/tpm.c
@@ -1983,18 +1983,15 @@ static CK_RV handle_ecparams(CK_ATTRIBUTE_PTR attr, CK_ULONG index, void *udata)
     UNUSED(index);
 
     tpm_key_data *keydat = (tpm_key_data *)udata;
-    const unsigned char *p = attr->pValue;
 
-    ASN1_OBJECT *a = d2i_ASN1_OBJECT(NULL, &p, attr->ulValueLen);
-    if (!a) {
-        LOGE("Unknown CKA_EC_PARAMS value");
-        return CKR_ATTRIBUTE_VALUE_INVALID;
+    int nid = 0;
+    CK_RV rv = ec_params_to_nid(attr, &nid);
+    if (rv != CKR_OK) {
+        return rv;
     }
 
     TPMS_ECC_PARMS *ec = &keydat->pub.publicArea.parameters.eccDetail;
 
-    int nid = OBJ_obj2nid(a);
-    ASN1_OBJECT_free(a);
     switch (nid) {
     case NID_X9_62_prime192v1:
         ec->curveID = TPM2_ECC_NIST_P192;

src/lib/utils.c
@@ -812,3 +812,19 @@ CK_RV utils_mech_free(CK_MECHANISM_PTR mechs, CK_ULONG mech_count, CK_MECHANISM_
 
     return utils_handle_mechs(mech_free_handlers, ARRAY_LEN(mech_free_handlers), mechs, mech_count, copy);
 }
+
+CK_RV ec_params_to_nid(CK_ATTRIBUTE_PTR ecparams, int *nid) {
+
+    const unsigned char *p = ecparams->pValue;
+
+    ASN1_OBJECT *a = d2i_ASN1_OBJECT(NULL, &p, ecparams->ulValueLen);
+    if (!a) {
+        LOGE("Unknown CKA_EC_PARAMS value");
+        return CKR_ATTRIBUTE_VALUE_INVALID;
+    }
+
+    * nid = OBJ_obj2nid(a);
+    ASN1_OBJECT_free(a);
+
+    return CKR_OK;
+}

src/lib/utils.h
@@ -161,4 +161,15 @@ CK_RV utils_attr_free(CK_ATTRIBUTE_PTR attrs, CK_ULONG attr_count);
         return CKR_OK; \
     }
 
+/**
+ * Given an attribute of CKA_EC_PARAMS returns the nid value.
+ * @param ecparams
+ *  The DER X9.62 parameters value
+ * @param nid
+ *  The nid to set
+ * @return
+ *  CKR_OK on success.
+ */
+CK_RV ec_params_to_nid(CK_ATTRIBUTE_PTR ecparams, int *nid);
+
 #endif /* SRC_PKCS11_UTILS_H_ */

commit 5dec3340063382ff5576d4d7ea7c919c6b2af5d1

Fix Buffer Size for ECDSA
The function ec_fixup_size was not implemented correctly,
as the initial logic whether the fixup should be applied was inverted.
Moreover the function was only called if do_hash was set to true,
which is not the case for 'raw' ECDSA.

Fixes: 62a521d4964c8af03d886920be52047b4d60fe44
Fixes: #201

Signed-off-by: Peter Huewe <peter.huewe@infineon.com>

src/lib/sign.c
@@ -96,8 +96,8 @@ static bool is_mech_supported(CK_MECHANISM_TYPE mech) {
 
 static CK_RV ec_fixup_size(CK_MECHANISM_TYPE mech, tobject *tobj, CK_ULONG_PTR signature_len) {
 
-    if (mech != CKM_ECDSA
-            || mech != CKM_ECDSA_SHA1) {
+    if (mech != CKM_ECDSA &&
+        mech != CKM_ECDSA_SHA1) {
         /* nothing to fix up */
         return CKR_OK;
     }
@@ -537,6 +537,14 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
      */
     reset_ctx = (rv == CKR_BUFFER_TOO_SMALL || !signature);
     if (reset_ctx) {
+        /* ec signature size is not stable between calls, fix it up */
+        CK_RV tmp; /* we must not overwrite rv */
+        tmp = ec_fixup_size(opdata->mtype, opdata->tobj, signature_len);
+        if (tmp != CKR_OK) {
+            reset_ctx = false;
+            goto session_out;
+        }
+
         if (opdata->do_hash) {
             /* reset the hashing state */
             digest_op_data *new_digest_state = digest_op_data_new();
@@ -548,7 +556,7 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
 
             assert(opdata->digest_opdata);
 
-            CK_RV tmp = digest_init_op(ctx, new_digest_state, opdata->digest_opdata->mechanism);
+            tmp = digest_init_op(ctx, new_digest_state, opdata->digest_opdata->mechanism);
             if (tmp != CKR_OK) {
                 digest_op_data_free(&new_digest_state);
                 reset_ctx = false;
@@ -557,14 +565,6 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
 
             digest_op_data_free(&opdata->digest_opdata);
             opdata->digest_opdata = new_digest_state;
-
-            /* ec signature size is not stable between calls, fix it up */
-            tmp = ec_fixup_size(opdata->mtype, opdata->tobj, signature_len);
-            if (tmp != CKR_OK) {
-                digest_op_data_free(&new_digest_state);
-                reset_ctx = false;
-                goto session_out;
-            }
 
         } else if (is_oneshot) {
             twist_free(opdata->buffer);

test/integration/pkcs-sign-verify.int.c
@@ -411,18 +411,29 @@ static void test_sign_verify_CKM_ECDSA(void **state) {
         0x67, 0x58, 0xae, 0x5a, 0xa3, 0x2e, 0x47, 0x0d
     };
 
-    CK_ULONG siglen = sizeof(sig);
+    CK_ULONG siglen;
+
+    /* Call C_Sign for size */
+    rv = C_Sign(session, sha256_msg_hash, sizeof(sha256_msg_hash), NULL,
+            &siglen);
+    assert_int_equal(rv, CKR_OK);
+    /* The signature comes back as DER encoded R + S parts of the signature.
+     * R + S is 2 times the curve size in bytes (so 64 for P256) but we're not
+     * returning that, but the DER encoded format that tools expect.
+     * Since the length of DER encoding is dependend on the encoded value
+     * (e.g. leading zero if negative), the output size is not stable.
+     * Thus calling C_Sign for size must return the maximum length of the DER
+     * encoded value, which is (2+1+keylength) * 2 + 2. So for P256 = 72
+     * the actual signature size may be smaller.
+     */
+    assert_int_equal(siglen, 72);
+    CK_ULONG tmp_len = siglen;
 
     rv = C_Sign(session, sha256_msg_hash, sizeof(sha256_msg_hash), sig,
             &siglen);
     assert_int_equal(rv, CKR_OK);
-    /*
-     * Skip checking the siglen. This comes back as a DER encoded R + S portions of the signature.
-     * R + S is 2 times the curve size in bytes (so 64 for P256) but we're not returning that, were
-     * returning the DER encoded format that tools expect, in DER format which may cause leading
-     * bytes to be dropped from, R + S, so the output size isn't stable. But it's definitely not 0.
-     */
-    assert_int_not_equal(siglen, 0);
+    /* the actual siglength may be smaller than the previously reported siglen */
+    assert_in_range(siglen, 1, tmp_len);
 
     /* try the public key verification */
     rv = C_VerifyInit(session, &mech, pubkey);
@@ -466,11 +477,20 @@ static void test_sign_verify_CKM_ECDSA_SHA1(void **state) {
     assert_int_equal(rv, CKR_OK);
 
     CK_BYTE ckm_ecdsa_sha1_sig[4096];
-    CK_ULONG ckm_ecdsa_sha1_siglen = sizeof(ckm_ecdsa_sha1_sig);
+    CK_ULONG ckm_ecdsa_sha1_siglen = 0;
 
+    /* Call C_Sign for Size */
+    rv = C_Sign(session, (CK_BYTE_PTR ) _data, sizeof(_data),
+            NULL, &ckm_ecdsa_sha1_siglen);
+    assert_int_equal(rv, CKR_OK);
+    assert_int_not_equal(ckm_ecdsa_sha1_siglen, 0);
+
+    CK_ULONG tmp_len = ckm_ecdsa_sha1_siglen;
     rv = C_Sign(session, (CK_BYTE_PTR ) _data, sizeof(_data),
             ckm_ecdsa_sha1_sig, &ckm_ecdsa_sha1_siglen);
     assert_int_equal(rv, CKR_OK);
+    /* actual size must not be larger than previously indicated */
+    assert_in_range(ckm_ecdsa_sha1_siglen, 1, tmp_len);
 
     rv = C_VerifyInit(session, &mech, objhandles[0]);
     assert_int_equal(rv, CKR_OK);