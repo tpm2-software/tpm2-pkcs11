871a3c5c94c53eb88e5d89a39ae4db70df8710a6
joholl <johannes.holland@infineon.com>
2019-07-19 14:42:50 +0200

    test: fix change of tpm2_evictcontrol options
    With 3c097b83, the option of tpm2_evictcontrol changed from -a to -C.
    
    Signed-off-by: joholl <johannes.holland@infineon.com>

--- test/integration/scripts/create_pkcs_store.sh
+++ test/integration/scripts/create_pkcs_store.sh
@@ -54,7 +54,7 @@ tpm2_ptool init --pobj-pin=mypobjpin --path=$TPM2_PKCS11_STORE
 
 # Test the existing primary object init functionality
 tpm2_createprimary -p foopass -o $TPM2_PKCS11_STORE/primary.ctx -g sha256 -G rsa
-handle=`tpm2_evictcontrol -a o -c $TPM2_PKCS11_STORE/primary.ctx | grep -Po '(?<=persistent-handle: )\S+'`
+handle=`tpm2_evictcontrol -C o -c $TPM2_PKCS11_STORE/primary.ctx | grep -Po '(?<=persistent-handle: )\S+'`
 
 tpm2_ptool init --pobj-pin=anotherpobjpin --primary-handle=$handle --primary-auth=foopass --path=$TPM2_PKCS11_STORE
 
