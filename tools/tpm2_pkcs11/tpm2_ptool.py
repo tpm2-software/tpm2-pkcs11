tpm2-software/tpm2-pkcs11

commit d5e54bd3c441c26afcbc876934869f3aa3129392

tpm2_ptool: suppress false-positive LGTM alerts
- "from .pkcs11t import *" produces a "import pollutes the enclosing
namespace" warning, but the module contains only constants that we want
to import.
- "tokid = db.addsecondary(tokid, encsobjauth, sobjpriv, sobjpub)"
produces an "assigned value is never used" warning, but using the syntax
makes sense here because it matches the preceeding line and avoids
errors if more code should be added afterwards.
- The "unused import" warnings in tpm2_ptool.py are already suppressed
for Pylint, disable them for lgtm.com as well.

"noqa" is a somewhat universal annotation also understood by other
linters (but unfortunately not by Pylint yet).

Signed-off-by: Jonas Witschel <diabonas@gmx.de>

tools/tpm2_pkcs11/tpm2_ptool.py
@@ -2,21 +2,21 @@ from .command import commandlet
 
 # These imports are required to add the commandlet even though they appear unused
 # Store level commands
-from .commandlets_store import InitCommand  # pylint: disable=unused-import
-from .commandlets_store import DestroyCommand  # pylint: disable=unused-import
+from .commandlets_store import InitCommand  # noqa # pylint: disable=unused-import
+from .commandlets_store import DestroyCommand  # noqa # pylint: disable=unused-import
 
 # Token Level Commands
-from .commandlets_token import AddTokenCommand  # pylint: disable=unused-import
-from .commandlets_token import AddEmptyTokenCommand  # pylint: disable=unused-import
-from .commandlets_token import RmTokenCommand  # pylint: disable=unused-import
+from .commandlets_token import AddTokenCommand  # noqa # pylint: disable=unused-import
+from .commandlets_token import AddEmptyTokenCommand  # noqa # pylint: disable=unused-import
+from .commandlets_token import RmTokenCommand  # noqa # pylint: disable=unused-import
 
-from .commandlets_token import VerifyCommand  # pylint: disable=unused-import
+from .commandlets_token import VerifyCommand  # noqa # pylint: disable=unused-import
 
-from .commandlets_token import InitPinCommand  # pylint: disable=unused-import
-from .commandlets_token import ChangePinCommand  # pylint: disable=unused-import
+from .commandlets_token import InitPinCommand  # noqa # pylint: disable=unused-import
+from .commandlets_token import ChangePinCommand  # noqa # pylint: disable=unused-import
 
-from .commandlets_keys import AddKeyCommand  # pylint: disable=unused-import
-from .commandlets_keys import ImportCommand  # pylint: disable=unused-import
+from .commandlets_keys import AddKeyCommand  # noqa # pylint: disable=unused-import
+from .commandlets_keys import ImportCommand  # noqa # pylint: disable=unused-import
 
 def main():
     '''The main entry point.'''

tools/tpm2_pkcs11/commandlets_keys.py
@@ -20,7 +20,7 @@ from .utils import asn1_format_ec_point_uncompressed
 
 from .tpm2 import Tpm2
 
-from .pkcs11t import *
+from .pkcs11t import * # noqa
 
 
 class NewKeyCommandBase(Command):

tools/tpm2_pkcs11/commandlets_token.py
@@ -372,7 +372,7 @@ class AddTokenCommand(Command):
                 tokid = db.addwrapping(tokid, wrappingobjpriv, wrappingobjpub)
 
             # Update the secondary object table
-            tokid = db.addsecondary(tokid, encsobjauth, sobjpriv, sobjpub)
+            tokid = db.addsecondary(tokid, encsobjauth, sobjpriv, sobjpub) # noqa
 
             print("Created token label: %s" % label)
 

commit 9dea3301ff79005511f654868c70269cc463904a

tpm2_ptool: move 'noqa' annotation to end of line
lgtm.com does not recognise the suppression comment if there is trailing
text after it, while Pylint does not mind a trailing comment, so move
'noqa' to the end to appease both Pylint and LGTM.

Fixes: d5e54bd3c441c26afcbc876934869f3aa3129392
Signed-off-by: Jonas Witschel <diabonas@gmx.de>

tools/tpm2_pkcs11/tpm2_ptool.py
@@ -2,21 +2,21 @@ from .command import commandlet
 
 # These imports are required to add the commandlet even though they appear unused
 # Store level commands
-from .commandlets_store import InitCommand  # noqa # pylint: disable=unused-import
-from .commandlets_store import DestroyCommand  # noqa # pylint: disable=unused-import
+from .commandlets_store import InitCommand  # pylint: disable=unused-import # noqa
+from .commandlets_store import DestroyCommand  # pylint: disable=unused-import # noqa
 
 # Token Level Commands
-from .commandlets_token import AddTokenCommand  # noqa # pylint: disable=unused-import
-from .commandlets_token import AddEmptyTokenCommand  # noqa # pylint: disable=unused-import
-from .commandlets_token import RmTokenCommand  # noqa # pylint: disable=unused-import
+from .commandlets_token import AddTokenCommand  # pylint: disable=unused-import # noqa
+from .commandlets_token import AddEmptyTokenCommand  # pylint: disable=unused-import # noqa
+from .commandlets_token import RmTokenCommand  # pylint: disable=unused-import # noqa
 
-from .commandlets_token import VerifyCommand  # noqa # pylint: disable=unused-import
+from .commandlets_token import VerifyCommand  # pylint: disable=unused-import # noqa
 
-from .commandlets_token import InitPinCommand  # noqa # pylint: disable=unused-import
-from .commandlets_token import ChangePinCommand  # noqa # pylint: disable=unused-import
+from .commandlets_token import InitPinCommand  # pylint: disable=unused-import # noqa
+from .commandlets_token import ChangePinCommand  # pylint: disable=unused-import # noqa
 
-from .commandlets_keys import AddKeyCommand  # noqa # pylint: disable=unused-import
-from .commandlets_keys import ImportCommand  # noqa # pylint: disable=unused-import
+from .commandlets_keys import AddKeyCommand  # pylint: disable=unused-import # noqa
+from .commandlets_keys import ImportCommand  # pylint: disable=unused-import # noqa
 
 def main():
     '''The main entry point.'''