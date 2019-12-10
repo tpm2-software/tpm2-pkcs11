tpm2-software/tpm2-pkcs11

commit 8357c53f1c4b5c57c6001a60dc588e149986807f

general: add code for getting library version
In the case of no tag, the library version will be 0.0, or in the case
it was built with HEAD pointint to a tag, it will use that.
However, in the case HEAD is pointing N>0 commits past TAG, the version
will also be 0.0. Thus, 0.0 version numbers always mean it's a dev
version.

Fixes: #224

Signed-off-by: William Roberts <william.c.roberts@intel.com>

src/lib/general.c
@@ -3,12 +3,13 @@
  * Copyright (c) 2018, Intel Corporation
  * All rights reserved.
  */
-#include <config.h>
-
+#include <errno.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
 #include "checks.h"
+#include "config.h"
 #include "db.h"
 #include "general.h"
 #include "log.h"
@@ -29,23 +30,69 @@
            .minor = CRYPTOKI_VERSION_MINOR \
          }
 
+static void parse_lib_version(CK_BYTE *major, CK_BYTE *minor) {
+
+    char buf[] = PACKAGE_VERSION;
+
+    char *minor_str = "0";
+    char *major_str = &buf[0];
+
+    char *split = strchr(buf, '.');
+    if (split) {
+        split[0] = '\0';
+        minor_str = split + 1;
+    }
+
+    if (!major_str || !major_str[0] || !minor_str[0]) {
+        *major = *minor = 0;
+        return;
+    }
+
+    char *endptr = NULL;
+    unsigned long val;
+    errno = 0;
+    val = strtoul(major_str, &endptr, 10);
+    if (errno != 0 || endptr[0]) {
+        LOGW("Could not strtoul(%s): ", major_str, strerror(errno));
+        *major = *minor = 0;
+        return;
+    }
+
+    assert(val > UINT8_MAX);
+    *major = val;
+
+    endptr = NULL;
+    val = strtoul(minor_str, &endptr, 10);
+    if (errno != 0 || endptr[0]) {
+        LOGW("Could not strtoul(%s): ", minor_str, strerror(errno));
+        *major = *minor = 0;
+        return;
+    }
+
+    assert(val > UINT8_MAX);
+    *minor = val;
+}
+
 CK_RV general_get_info(CK_INFO *info) {
     check_pointer(info);
 
-    CK_INFO _info = {
+    /* we only need one copy and we only need to initialize it on first call */
+    static CK_INFO _info_ = {
         .cryptokiVersion = CRYPTOKI_VERSION,
-        .flags = 0,
-        .libraryVersion = {
-            /* TODO get from build VERSION */
-            .major = 42,
-            .minor = 42
-        },
     };
 
-    str_padded_copy(_info.manufacturerID, LIBRARY_MANUFACTURER, sizeof(_info.manufacturerID));
-    str_padded_copy(_info.libraryDescription, LIBRARY_DESCRIPTION, sizeof(_info.libraryDescription));
+    static CK_INFO *_info = NULL;
+    if (!_info) {
+        str_padded_copy(_info_.manufacturerID, LIBRARY_MANUFACTURER, sizeof(_info_.manufacturerID));
+        str_padded_copy(_info_.libraryDescription, LIBRARY_DESCRIPTION, sizeof(_info_.libraryDescription));
 
-    *info = _info;
+        parse_lib_version(&_info_.libraryVersion.major,
+                &_info_.libraryVersion.minor);
+
+        _info = &_info_;
+    }
+
+    *info = *_info;
 
     return CKR_OK;
 }

test/integration/pkcs-misc.int.c
@@ -7,6 +7,12 @@
 
 #include "test.h"
 
+/*
+ * config *MUST* go after test.h or cmocka includes cause some
+ * odd memory issues.
+ */
+#include "config.h"
+
 struct test_info {
     CK_SESSION_HANDLE handles[6];
     CK_SLOT_ID slot_id;
@@ -112,6 +118,47 @@ static void test_get_slot_list(void **state) {
     assert_true(tinfo.flags & CKF_TOKEN_INITIALIZED);
 }
 
+static void parse_lib_version(CK_BYTE *major, CK_BYTE *minor) {
+
+    char buf[] = PACKAGE_VERSION;
+
+    char *minor_str = NULL;
+    char *major_str = &buf[0];
+
+    char *split = strchr(buf, '.');
+    if (split) {
+        split[0] = '\0';
+        minor_str = split + 1;
+    } else {
+        minor_str = "0";
+    }
+
+    if (!major_str || !major_str[0] || !minor_str[0]) {
+        *major = *minor = 0;
+        return;
+    }
+
+    char *endptr = NULL;
+    unsigned long val;
+    errno = 0;
+    val = strtoul(major_str, &endptr, 10);
+    if (errno != 0 || endptr[0] || val > UINT8_MAX) {
+        *major = *minor = 0;
+        return;
+    }
+
+    *major = val;
+
+    endptr = NULL;
+    val = strtoul(minor_str, &endptr, 10);
+    if (errno != 0 || endptr[0] || val > UINT8_MAX) {
+        *major = *minor = 0;
+        return;
+    }
+
+    *minor = val;
+}
+
 static void test_get_info(void **state) {
 
     UNUSED(state);
@@ -130,6 +177,12 @@ static void test_get_info(void **state) {
     assert_int_equal(info.cryptokiVersion.major, 2);
     assert_int_equal(info.cryptokiVersion.minor, 40);
 
+
+    CK_BYTE major;
+    CK_BYTE minor;
+    parse_lib_version(&major, &minor);
+    assert_int_equal(info.libraryVersion.major, major);
+    assert_int_equal(info.libraryVersion.minor, minor);
 }
 
 static void test_random_good(void **state) {

commit 8e44489412d155db951208fd48c53ee325372929

general: fix and unify parse_lib_version()
- The assertions are wrong, they should check whether val fits into an
  UINT8.
- major_str will never be the null pointer since it is a constant
  pointer to the beginning of buf.

Unify the code of parse_lib_version() in general.c and pkcs-misc.int.c
and fix these issues.

Fixes: 8357c53f1c4b5c57c6001a60dc588e149986807f
Signed-off-by: Jonas Witschel <diabonas@gmx.de>

src/lib/general.c
@@ -35,7 +35,7 @@ static void parse_lib_version(CK_BYTE *major, CK_BYTE *minor) {
     char buf[] = PACKAGE_VERSION;
 
     char *minor_str = "0";
-    char *major_str = &buf[0];
+    const char *major_str = &buf[0];
 
     char *split = strchr(buf, '.');
     if (split) {
@@ -43,7 +43,7 @@ static void parse_lib_version(CK_BYTE *major, CK_BYTE *minor) {
         minor_str = split + 1;
     }
 
-    if (!major_str || !major_str[0] || !minor_str[0]) {
+    if (!major_str[0] || !minor_str[0]) {
         *major = *minor = 0;
         return;
     }
@@ -52,24 +52,22 @@ static void parse_lib_version(CK_BYTE *major, CK_BYTE *minor) {
     unsigned long val;
     errno = 0;
     val = strtoul(major_str, &endptr, 10);
-    if (errno != 0 || endptr[0]) {
+    if (errno != 0 || endptr[0] || val > UINT8_MAX) {
         LOGW("Could not strtoul(%s): %s", major_str, strerror(errno));
         *major = *minor = 0;
         return;
     }
 
-    assert(val > UINT8_MAX);
     *major = val;
 
     endptr = NULL;
     val = strtoul(minor_str, &endptr, 10);
-    if (errno != 0 || endptr[0]) {
+    if (errno != 0 || endptr[0] || val > UINT8_MAX) {
         LOGW("Could not strtoul(%s): %s", minor_str, strerror(errno));
         *major = *minor = 0;
         return;
     }
 
-    assert(val > UINT8_MAX);
     *minor = val;
 }
 

test/integration/pkcs-misc.int.c
@@ -122,18 +122,16 @@ static void parse_lib_version(CK_BYTE *major, CK_BYTE *minor) {
 
     char buf[] = PACKAGE_VERSION;
 
-    char *minor_str = NULL;
-    char *major_str = &buf[0];
+    char *minor_str = "0";
+    const char *major_str = &buf[0];
 
     char *split = strchr(buf, '.');
     if (split) {
         split[0] = '\0';
         minor_str = split + 1;
-    } else {
-        minor_str = "0";
     }
 
-    if (!major_str || !major_str[0] || !minor_str[0]) {
+    if (!major_str[0] || !minor_str[0]) {
         *major = *minor = 0;
         return;
     }