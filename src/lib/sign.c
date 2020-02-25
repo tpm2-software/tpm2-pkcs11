tpm2-software/tpm2-pkcs11

commit d80809ac3b8267f346f90bc3c73275bd8c66456e

mechanism: refactor mechanism interface into common
Mechanisms have been a sore point for some time, with the CKM_
definitions littered about the code. Start to migrate the mechanism
handling code into a common spot, similair to the attribute handling.

This commit removed all direct CKM_ dependent checks from the C_Sign
logic.

Fixes: #404

Signed-off-by: William Roberts <william.c.roberts@intel.com>

src/lib/sign.c
@@ -4,14 +4,12 @@
 #include <assert.h>
 #include <stdlib.h>
 
-#include <openssl/rand.h>
-#include <openssl/rsa.h>
-
 #include "attrs.h"
 #include "checks.h"
 #include "digest.h"
 #include "encrypt.h"
 #include "log.h"
+#include "mech.h"
 #include "session.h"
 #include "session_ctx.h"
 #include "sign.h"
@@ -24,7 +22,7 @@ struct sign_opdata {
     bool do_hash;
     twist buffer;
     digest_op_data *digest_opdata;
-    encrypt_op_data *encrypt_opdata;
+    encrypt_op_data *crypto_opdata;
 };
 
 static sign_opdata *sign_opdata_new(void) {
@@ -38,102 +36,13 @@ static void sign_opdata_free(sign_opdata **opdata) {
         twist_free((*opdata)->buffer);
     }
 
+    if ((*opdata)->crypto_opdata) {
+        encrypt_op_data_free(&(*opdata)->crypto_opdata);
+    }
+
     free(*opdata);
 
     *opdata = NULL;
-}
-
-static CK_RV ec_fixup_size(CK_MECHANISM_TYPE mech, tobject *tobj, CK_ULONG_PTR signature_len) {
-
-    if (mech != CKM_ECDSA &&
-        mech != CKM_ECDSA_SHA1) {
-        /* nothing to fix up */
-        return CKR_OK;
-    }
-
-    CK_ATTRIBUTE_PTR a = attr_get_attribute_by_type(tobj->attrs, CKA_EC_PARAMS);
-    assert(a);
-
-    int nid = 0;
-    CK_RV rv = ec_params_to_nid(a, &nid);
-    if (rv != CKR_OK) {
-        return rv;
-    }
-
-    /*
-     * Math below is based off of ECDSA signature:
-     * SEQUENCE (2 elem)
-     *  INTEGER R
-     *  INTEGER S
-     *
-     *  Integers R and S are bounded by keysize in bytes, followed by their
-     *  respective headers(2bytes) followed by the SEQUENCE header(2 bytes)
-     */
-    unsigned keysize;
-
-    switch (nid) {
-    case NID_X9_62_prime192v1:
-        keysize = 24;
-    break;
-    case NID_secp224r1:
-        keysize = 28;
-    break;
-    case NID_X9_62_prime256v1:
-        keysize = 32;
-    break;
-    case NID_secp384r1:
-        keysize = 48;
-    break;
-    case NID_secp521r1:
-        keysize = 66; /* round up */
-    break;
-    default:
-        LOGE("Unsupported nid to tpm signature size maaping: %d", nid);
-        return CKR_CURVE_NOT_SUPPORTED;
-    }
-
-    /* R and S are INTEGER objects with a header and len byte */
-    static const unsigned INT_HDR = 2U;
-    /* R and S are combined in a SEQUENCE object with a header and len byte */
-    static const unsigned SEQ_HDR = 2U;
-    /* an R or S with a high bit set needs an extra nul byte so it's not negative (twos comp)*/
-    static const unsigned EXTRA = 1U;
-
-    unsigned tmp = ((keysize + INT_HDR + EXTRA) * 2); /* x2 1 for R and 1 for S */
-
-    tmp += SEQ_HDR;
-
-    *signature_len = tmp;
-
-    return CKR_OK;
-}
-
-/*XXX Perhaps create a mechanism checking interface so we can get all
- * this scattered mechanism logic to a single spot.
- */
-static bool is_hashing_needed(CK_MECHANISM_TYPE mech) {
-
-   switch(mech) {
-     case CKM_SHA1_RSA_PKCS:
-     case CKM_SHA256_RSA_PKCS:
-     case CKM_SHA384_RSA_PKCS:
-     case CKM_SHA512_RSA_PKCS:
-     case CKM_SHA1_RSA_PKCS_PSS:
-     case CKM_SHA256_RSA_PKCS_PSS:
-     case CKM_SHA384_RSA_PKCS_PSS:
-     case CKM_SHA512_RSA_PKCS_PSS:
-     case CKM_ECDSA_SHA1:
-         return true;
-     case CKM_ECDSA:
-     case CKM_RSA_X_509:
-     case CKM_RSA_PKCS:
-     case CKM_RSA_PKCS_PSS:
-         return false;
-    default:
-        LOGW("Unknown mech: %lu", mech);
-    }
-
-    return false;
 }
 
 static CK_RV common_init(operation op, session_ctx *ctx, CK_MECHANISM_PTR mechanism, CK_OBJECT_HANDLE key) {
@@ -159,15 +68,19 @@ static CK_RV common_init(operation op, session_ctx *ctx, CK_MECHANISM_PTR mechan
         return rv;
     }
 
-    rv = object_mech_is_supported(tobj, mechanism);
+    rv = mech_validate(tok->tctx, mechanism, tobj->attrs);
     if (rv != CKR_OK) {
         return rv;
     }
 
     digest_op_data *digest_opdata = NULL;
-    bool do_hash = is_hashing_needed(mechanism->mechanism);
+    bool is_hashing_needed = false;
+    rv = mech_is_hashing_needed(mechanism, &is_hashing_needed);
+    if (rv != CKR_OK) {
+        return rv;
+    }
 
-    if (do_hash) {
+    if (is_hashing_needed) {
 
         digest_opdata = digest_op_data_new();
         if (!digest_opdata) {
@@ -181,14 +94,29 @@ static CK_RV common_init(operation op, session_ctx *ctx, CK_MECHANISM_PTR mechan
         }
     }
 
+    tpm_op_data *tpm_opdata;
+    rv = mech_get_tpm_opdata(tok->tctx, mechanism, tobj, &tpm_opdata);
+    if (rv != CKR_OK) {
+        tpm_opdata_free(&tpm_opdata);
+        return rv;
+    }
+
     sign_opdata *opdata = sign_opdata_new();
     if (!opdata) {
         return CKR_HOST_MEMORY;
     }
 
-    opdata->do_hash = do_hash;
+    opdata->do_hash = is_hashing_needed;
     memcpy(&opdata->mech, mechanism, sizeof(opdata->mech));
     opdata->digest_opdata = digest_opdata;
+
+    opdata->crypto_opdata = encrypt_op_data_new(tobj);
+    if (!opdata->crypto_opdata) {
+        sign_opdata_free(&opdata);
+        return CKR_HOST_MEMORY;
+    }
+
+    opdata->crypto_opdata->cryptopdata.tpm_opdata = tpm_opdata;
 
     /*
      * Store everything for later
@@ -241,112 +169,6 @@ CK_RV sign_update(session_ctx *ctx, CK_BYTE_PTR part, CK_ULONG part_len) {
     return common_update(operation_sign, ctx, part, part_len);
 }
 
-static CK_RV pkcs1_5_build_struct(CK_MECHANISM_TYPE mech,
-        CK_BYTE_PTR hash, CK_ULONG hash_len,
-        char **built, size_t *built_len) {
-
-    /* These headers are defined in the following RFC
-     *   - https://www.ietf.org/rfc/rfc3447.txt
-     *     - Page 42
-     */
-    static const CK_BYTE pkcs1_5_hdr_sha1[15] = {
-        0x30, 0x21, 0x30, 0x09, 0x06, 0x05, 0x2b, 0x0e, 0x03, 0x02, 0x1a,
-        0x05, 0x00, 0x04, 0x14,
-    };
-
-    static const CK_BYTE pkcs1_5_hdr_sha256[19] = {
-        0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03,
-        0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20,
-    };
-
-    static const CK_BYTE pkcs1_5_hdr_sha384[19] = {
-        0x30, 0x41, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03,
-        0x04, 0x02, 0x02, 0x05, 0x00, 0x04, 0x30,
-    };
-
-    static const CK_BYTE pkcs1_5_hdr_sha512[19] = {
-        0x30, 0x51, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03,
-        0x04, 0x02, 0x03, 0x05, 0x00, 0x04, 0x40,
-    };
-
-    const CK_BYTE *hdr;
-    size_t hdr_size;
-
-    switch(mech) {
-    case CKM_SHA1_RSA_PKCS:
-        hdr = pkcs1_5_hdr_sha1;
-        hdr_size = sizeof(pkcs1_5_hdr_sha1);
-        break;
-    case CKM_SHA256_RSA_PKCS:
-        hdr = pkcs1_5_hdr_sha256;
-        hdr_size = sizeof(pkcs1_5_hdr_sha256);
-        break;
-    case CKM_SHA384_RSA_PKCS:
-        hdr = pkcs1_5_hdr_sha384;
-        hdr_size = sizeof(pkcs1_5_hdr_sha384);
-        break;
-    case CKM_SHA512_RSA_PKCS:
-        hdr = pkcs1_5_hdr_sha512;
-        hdr_size = sizeof(pkcs1_5_hdr_sha512);
-        break;
-    default:
-        return CKR_MECHANISM_INVALID;
-    }
-
-    /*
-     * Build and populate a buffer with hdr + hash
-     */
-    char *b = calloc(1, hdr_size + hash_len);
-    if (!b) {
-        return CKR_HOST_MEMORY;
-    }
-
-    memcpy(b, hdr, hdr_size);
-    memcpy(&b[hdr_size], hash, hash_len);
-
-    *built_len = hdr_size + hash_len;
-    *built = b;
-
-    return CKR_OK;
-}
-
-static CK_RV apply_pkcs_1_5_pad(tobject *tobj, char *built, size_t built_len, char **padded, size_t *padded_len) {
-
-    CK_ATTRIBUTE_PTR a = attr_get_attribute_by_type(tobj->attrs, CKA_MODULUS_BITS);
-    if (!a) {
-        LOGE("Signing key has no CKA_MODULUS_BITS");
-        return CKR_GENERAL_ERROR;
-    }
-
-    if (a->ulValueLen != sizeof(CK_ULONG)) {
-        LOGE("Modulus bit pointer data not size of CK_ULONG, got %lu, expected %zu",
-                a->ulValueLen, sizeof(CK_ULONG));
-        return CKR_GENERAL_ERROR;
-    }
-
-    CK_ULONG_PTR keybits = (CK_ULONG_PTR)a->pValue;
-
-    size_t out_len = *keybits / 8;
-
-    char *out = malloc(out_len);
-    if (!out) {
-        LOGE("oom");
-        return CKR_HOST_MEMORY;
-    }
-
-    /* Apply the PKCS1.5 padding */
-    int rc = RSA_padding_add_PKCS1_type_1((CK_BYTE_PTR )out, out_len,
-            (const CK_BYTE_PTR )built, built_len);
-    if (!rc) {
-        LOGE("Applying RSA padding failed");
-    }
-
-    *padded = out;
-    *padded_len = out_len;
-
-    return CKR_OK;
-}
-
 CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signature_len, bool is_oneshot) {
 
     check_pointer(signature_len);
@@ -354,9 +176,6 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
     bool reset_ctx = false;
 
     CK_RV rv = CKR_GENERAL_ERROR;
-
-    CK_BYTE_PTR hash = NULL;
-    CK_ULONG hash_len = 0;
 
     sign_opdata *opdata = NULL;
     rv = session_ctx_opdata_get(ctx, operation_sign, &opdata);
@@ -373,105 +192,76 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
     token *tok = session_ctx_get_token(ctx);
     assert(tok);
 
-    tpm_ctx *tpm = tok->tctx;
-
     tobject *tobj = session_ctx_opdata_get_tobject(ctx);
     assert(tobj);
 
+    twist digest_buf = NULL;
+
+    size_t tmp_len = 0;
+    rv = tobject_get_max_buf_size(tobj, &tmp_len);
+    if (rv != CKR_OK) {
+        return rv;
+    }
+
+    if (!signature) {
+        *signature_len = tmp_len;
+        goto out;
+    }
+
+    if (*signature_len < tmp_len) {
+        *signature_len = tmp_len;
+        rv = CKR_BUFFER_TOO_SMALL;
+        goto out;
+    }
+
     if (opdata->do_hash) {
 
-        hash_len = utils_get_halg_size(opdata->mech.mechanism);
+        size_t hash_len = utils_get_halg_size(opdata->mech.mechanism);
         if (!hash_len) {
             LOGE("Hash algorithm cannot have 0 size");
             return CKR_GENERAL_ERROR;
         }
-        hash = malloc(hash_len);
-        if (!hash) {
+        digest_buf = twist_calloc(hash_len);
+        if (!digest_buf) {
             LOGE("oom");
             rv = CKR_HOST_MEMORY;
             goto session_out;
         }
 
-        rv = digest_final_op(ctx, opdata->digest_opdata, hash, &hash_len);
+        rv = digest_final_op(ctx, opdata->digest_opdata, (CK_BYTE_PTR)digest_buf, &hash_len);
         if (rv != CKR_OK) {
             goto session_out;
         }
+    } else {
+        digest_buf = opdata->buffer;
+        /* we take ownership of this buffer */
+        opdata->buffer = NULL;
     }
 
-    /*
-     * Their are two cases when we need to use the raw RSA Decrypt to sign the signature:
-     *
-     * CASE 1
-     * In the case of CKM_RSA_PKCS the raw DigestInfo structure has been done off-card, just perform
-     * a an RSA PKCS1.5 padded private-key encryption formally known as RSA decrypt.
-     *
-     * CASE 2
-     * This method should also be used if the TPM doesn't support the hash algorithm, ie hash off card,
-     * build digest info ASN1 structure, apply padding and RSA_Decrypt() AND the signing structure
-     * is PKCS1.5
-     */
+    CK_BYTE syn_buf[4096];
+    CK_ULONG syn_buf_len = sizeof(syn_buf);
+    CK_ULONG digest_buf_len = twist_len(digest_buf);
 
-    bool is_raw_sign = utils_mech_is_raw_sign(opdata->mech.mechanism);
-    bool is_pkcs1_5 = utils_mech_is_rsa_pkcs(opdata->mech.mechanism);
+    rv = mech_synthesize(tok->tctx,
+            &opdata->mech, tobj->attrs,
+            (CK_BYTE_PTR)digest_buf, digest_buf_len,
+            syn_buf, &syn_buf_len);
+    if (rv != CKR_OK) {
+        goto session_out;
+    }
 
-    if (is_raw_sign || (is_pkcs1_5 && opdata->do_hash)) {
+    bool is_synthetic = false;
+    rv = mech_is_synthetic(tok->tctx, &opdata->mech,
+            &is_synthetic);
+    if (rv != CKR_OK) {
+        goto session_out;
+    }
 
-        char *built = NULL;
-        size_t built_len = 0;
-
-        if(opdata->do_hash) {
-            /*
-             * Ok we did the hash, AND because of the entry condition, it's a SW hash, as is_raw_sign
-             * means we didn't do the hashing. In this case, hash and hash_len should be set with
-             * the digest.
-             */
-            assert(hash);
-            assert(hash_len);
-            assert(!opdata->buffer);
-
-            rv = pkcs1_5_build_struct(opdata->mech.mechanism, hash, hash_len, &built, &built_len);
-            if (rv != CKR_OK) {
-                goto session_out;
-            }
-
-        } else {
-
-            /*
-             * We just use the appended buffer, but make a copy to make the free logic easier.
-             */
-            assert(!hash);
-            assert(!hash_len);
-            assert(opdata->buffer);
-
-            built = malloc(twist_len(opdata->buffer));
-            if (!built) {
-                goto session_out;
-            }
-            built_len = twist_len(opdata->buffer);
-            memcpy(built, opdata->buffer, built_len);
-        }
-
-        char *padded = NULL;
-        size_t padded_len = 0;
-
-        if (is_pkcs1_5) {
-            /* apply padding */
-            rv = apply_pkcs_1_5_pad(tobj, built, built_len, &padded, &padded_len);
-            free(built);
-            if (rv != CKR_OK) {
-                goto session_out;
-            }
-        } else {
-            padded = built;
-            padded_len = built_len;
-            built = NULL;
-            built_len = 0;
-        }
+    if (is_synthetic) {
 
         /* sign padded pkcs 1.5 structure */
         encrypt_op_data *encrypt_opdata = encrypt_op_data_new(tobj);
         if (!encrypt_opdata) {
-            free(padded);
             rv = CKR_HOST_MEMORY;
             goto session_out;
         }
@@ -484,13 +274,11 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
         /* RSA Decrypt is the RSA operation with the private key, which is what we want */
         rv = decrypt_init_op(ctx, encrypt_opdata, &mechanism, tobj->obj_handle);
         if (rv != CKR_OK) {
-            free(padded);
             encrypt_op_data_free(&encrypt_opdata);
             goto session_out;
         }
 
-        rv = decrypt_oneshot_op(ctx, encrypt_opdata, (CK_BYTE_PTR)padded, padded_len, signature, signature_len);
-        free(padded);
+        rv = decrypt_oneshot_op(ctx, encrypt_opdata, syn_buf, syn_buf_len, signature, signature_len);
         encrypt_op_data_free(&encrypt_opdata);
         if (rv != CKR_OK && rv != CKR_BUFFER_TOO_SMALL) {
             goto session_out;
@@ -509,39 +297,16 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
             goto session_out;
         }
     } else {
-
-        /*
-         * CKM_ECDSA is never considered a "raw sign" since the TPM natively supports it
-         * by setting the hashalg to TPM2_ALG_NULL. So just make sure that we propagate
-         * the raw data provided (hash) into the hash variable and perform a sign.
-         */
-        if (opdata->mech.mechanism == CKM_ECDSA ||
-                opdata->mech.mechanism == CKM_RSA_PKCS_PSS){
-            assert(!hash);
-            assert(!hash_len);
-            assert(opdata->buffer);
-            hash_len = twist_len(opdata->buffer);
-            assert(hash_len >= 20); /* Minimum for SHA1 */
-            hash = malloc(hash_len);
-            if (!hash) {
-                LOGE("oom");
-                rv = CKR_HOST_MEMORY;
-                goto session_out;
-            }
-
-            memcpy(hash, opdata->buffer, hash_len);
-        }
-
-        assert(hash);
-        assert(hash_len);
-        rv = tpm_sign(tpm, tobj, &opdata->mech, hash, hash_len, signature, signature_len);
+        rv = tpm_sign(opdata->crypto_opdata->cryptopdata.tpm_opdata,
+                syn_buf, syn_buf_len, signature, signature_len);
         if (rv != CKR_OK && rv != CKR_BUFFER_TOO_SMALL) {
             goto session_out;
         }
     }
 
+out:
     /*
-     * Detect 1 of 3 states:
+     * Detect 1 of 2 states:
      *   - 1 - everything is ok from enc/sign and sig is set (continue normally)
      *   - 2 - buffer too small from enc/sign and sig is set (reset hashing state and keep sign operation alive)
      *   - 3 - everything is ok but sig is NULL, handle like state 2.
@@ -550,14 +315,6 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
      */
     reset_ctx = (rv == CKR_BUFFER_TOO_SMALL || !signature);
     if (reset_ctx) {
-        /* ec signature size is not stable between calls, fix it up */
-        CK_RV tmp; /* we must not overwrite rv */
-        tmp = ec_fixup_size(opdata->mech.mechanism, tobj, signature_len);
-        if (tmp != CKR_OK) {
-            reset_ctx = false;
-            goto session_out;
-        }
-
         if (opdata->do_hash) {
             /* reset the hashing state */
             digest_op_data *new_digest_state = digest_op_data_new();
@@ -569,7 +326,7 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
 
             assert(opdata->digest_opdata);
 
-            tmp = digest_init_op(ctx, new_digest_state, opdata->digest_opdata->mechanism);
+            CK_RV tmp = digest_init_op(ctx, new_digest_state, opdata->digest_opdata->mechanism);
             if (tmp != CKR_OK) {
                 digest_op_data_free(&new_digest_state);
                 reset_ctx = false;
@@ -589,7 +346,7 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
     }
 
 session_out:
-
+    twist_free(digest_buf);
     assert(tobj);
     if (!reset_ctx) {
         tobj->is_authenticated = false;
@@ -597,13 +354,10 @@ session_out:
         if (tmp_rv != CKR_OK && rv == CKR_OK) {
             rv = tmp_rv;
         }
-    }
 
-    if (!reset_ctx) {
+        encrypt_op_data_free(&opdata->crypto_opdata);
         session_ctx_opdata_clear(ctx);
     }
-
-    free(hash);
 
     return rv;
 }
@@ -648,11 +402,6 @@ CK_RV verify_final (session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG signature_
     tobject *tobj = session_ctx_opdata_get_tobject(ctx);
     assert(tobj);
 
-    token *tok = session_ctx_get_token(ctx);
-    assert(tok);
-
-    tpm_ctx *tpm = tok->tctx;
-
     // TODO mode to buffer size
     CK_BYTE hash[1024];
     CK_ULONG hash_len = sizeof(hash);
@@ -674,7 +423,8 @@ CK_RV verify_final (session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG signature_
         memcpy(hash, opdata->buffer, datalen);
     }
 
-    rv = tpm_verify(tpm, tobj, &opdata->mech, hash, hash_len, signature, signature_len);
+    rv = tpm_verify(opdata->crypto_opdata->cryptopdata.tpm_opdata,
+            hash, hash_len, signature, signature_len);
 
 out:
     assert(tobj);
@@ -683,6 +433,8 @@ out:
     if (tmp_rv != CKR_OK && rv == CKR_OK) {
         rv = tmp_rv;
     }
+
+    encrypt_op_data_free(&opdata->crypto_opdata);
 
     session_ctx_opdata_clear(ctx);
 

src/lib/digest.c
@@ -7,6 +7,7 @@
 
 #include "checks.h"
 #include "digest.h"
+#include "mech.h"
 #include "session.h"
 #include "session_ctx.h"
 #include "token.h"
@@ -34,51 +35,15 @@ void digest_op_data_free(digest_op_data **opdata) {
     *opdata = NULL;
 }
 
-static const EVP_MD *ossl_halg_from_mech(CK_MECHANISM_TYPE mech) {
-
-    switch(mech) {
-        case CKM_SHA_1:
-            /* falls-thru */
-        case CKM_ECDSA_SHA1:
-            /* falls-thru */
-        case CKM_SHA1_RSA_PKCS:
-            /* falls-thru */
-        case CKM_SHA1_RSA_PKCS_PSS:
-            return EVP_sha1();
-        case CKM_SHA256:
-            /* falls-thru */
-        case CKM_SHA256_RSA_PKCS:
-            /* falls-thru */
-        case CKM_SHA256_RSA_PKCS_PSS:
-            return EVP_sha256();
-        case CKM_SHA384:
-            /* falls-thru */
-        case CKM_SHA384_RSA_PKCS:
-            /* falls-thru */
-        case CKM_SHA384_RSA_PKCS_PSS:
-            return EVP_sha384();
-        case CKM_SHA512:
-            /* falls-thru */
-        case CKM_SHA512_RSA_PKCS:
-            /* falls-thru */
-        case CKM_SHA512_RSA_PKCS_PSS:
-            return EVP_sha512();
-        default:
-            return NULL;
-    }
-    /* no return, not possible */
-}
-
-bool digest_is_supported(CK_MECHANISM_TYPE type) {
-    return ossl_halg_from_mech(type) != NULL;
-}
-
-
 static CK_RV digest_sw_init(digest_op_data *opdata) {
 
-    const EVP_MD *md = ossl_halg_from_mech(opdata->mechanism);
-    if (!md) {
-        return CKR_MECHANISM_INVALID;
+    CK_MECHANISM m = { 0 };
+    m.mechanism = opdata->mechanism;
+
+    const EVP_MD *md = NULL;
+    CK_RV rv = mech_get_digester(&m, &md);
+    if (rv != CKR_OK) {
+        return rv;
     }
 
     EVP_MD_CTX *mdctx = EVP_MD_CTX_create();

src/lib/digest.h
@@ -19,8 +19,6 @@ struct digest_op_data {
     EVP_MD_CTX *mdctx;
 };
 
-bool digest_is_supported(CK_MECHANISM_TYPE type);
-
 digest_op_data *digest_op_data_new(void);
 void digest_op_data_free(digest_op_data **opdata);
 

src/lib/encrypt.c
@@ -6,6 +6,7 @@
 
 #include "checks.h"
 #include "encrypt.h"
+#include "mech.h"
 #include "openssl_compat.h"
 #include "session.h"
 #include "session_ctx.h"
@@ -24,23 +25,24 @@ static sw_encrypt_data *sw_encrypt_data_new(void) {
     return (sw_encrypt_data *)calloc(1, sizeof(sw_encrypt_data));
 }
 
-static void sw_encrypt_data_free(sw_encrypt_data *enc_data) {
+static void sw_encrypt_data_free(sw_encrypt_data **enc_data) {
     if (!enc_data) {
         return;
     }
 
-    if (enc_data->key) {
-        RSA_free(enc_data->key);
+    if ((*enc_data)->key) {
+        RSA_free((*enc_data)->key);
     }
 
-    free(enc_data);
+    free(*enc_data);
+    *enc_data = NULL;
 }
 
 encrypt_op_data *encrypt_op_data_new(tobject *tobj) {
 
     CK_ATTRIBUTE_PTR a = attr_get_attribute_by_type(tobj->attrs, CKA_CLASS);
     if (!a) {
-        LOGE("Expected tobjects to have attribuet CKA_CLASS");
+        LOGE("Expected tobjects to have attribute CKA_CLASS");
         return NULL;
     }
 
@@ -65,8 +67,8 @@ void encrypt_op_data_free(encrypt_op_data **opdata) {
 
     if (opdata) {
         (*opdata)->use_sw ?
-                sw_encrypt_data_free((*opdata)->cryptopdata.sw_enc_data) :
-                tpm_encrypt_data_free((*opdata)->cryptopdata.tpm_enc_data);
+                sw_encrypt_data_free(&(*opdata)->cryptopdata.sw_enc_data) :
+                tpm_opdata_free(&(*opdata)->cryptopdata.tpm_opdata);
         free(*opdata);
         *opdata = NULL;
     }
@@ -245,7 +247,7 @@ out:
     return rv;
 }
 
-static CK_RV common_init_op (session_ctx *ctx, encrypt_op_data *supplied_opdata, operation op, CK_MECHANISM *mechanism, CK_OBJECT_HANDLE key) {
+static CK_RV common_init_op (session_ctx *ctx, encrypt_op_data *supplied_opdata, operation op, CK_MECHANISM_PTR mechanism, CK_OBJECT_HANDLE key) {
 
     check_pointer(mechanism);
 
@@ -294,8 +296,8 @@ static CK_RV common_init_op (session_ctx *ctx, encrypt_op_data *supplied_opdata,
      * only object and don't go to the TPM.
      */
     if (tobj->pub) {
-       rv = tpm_encrypt_data_init(tok->tctx, tobj->tpm_handle, tobj->unsealed_auth, mechanism,
-               &opdata->cryptopdata.tpm_enc_data);
+        rv = mech_get_tpm_opdata(tok->tctx, mechanism, tobj,
+                &opdata->cryptopdata.tpm_opdata);
     } else {
         opdata->use_sw = true;
         rv = sw_encrypt_data_init(mechanism, tobj, &opdata->cryptopdata.sw_enc_data);
@@ -303,7 +305,9 @@ static CK_RV common_init_op (session_ctx *ctx, encrypt_op_data *supplied_opdata,
 
     if (rv != CKR_OK) {
         tobject_user_decrement(tobj);
-        encrypt_op_data_free(&opdata);
+        if (!supplied_opdata) {
+            encrypt_op_data_free(&opdata);
+        }
         return rv;
     }
 

src/lib/encrypt.h
@@ -15,7 +15,7 @@ typedef struct encrypt_op_data encrypt_op_data;
 
 typedef union crypto_op_data crypto_op_data;
 union crypto_op_data{
-    tpm_encrypt_data *tpm_enc_data;
+    tpm_op_data *tpm_opdata;
     sw_encrypt_data *sw_enc_data;
 };
 

src/lib/mech.h
@@ -1,0 +1,49 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+#include <openssl/evp.h>
+
+#include "attrs.h"
+#include "object.h"
+#include "pkcs11.h"
+#include "tpm.h"
+
+#ifndef SRC_LIB_MECH_H_
+#define SRC_LIB_MECH_H_
+
+/**
+ * Validate that a mechanism is supported by the object/tpm.
+ *
+ * @param ctx
+ *  A context to the tpm.
+ * @param mech
+ *  The mechanism parameter the application is requesting.
+ * @param attrs
+ *  The attributes list / template of the object being used for the operation.
+ * @return
+ */
+CK_RV mech_validate(tpm_ctx *ctx, CK_MECHANISM_PTR mech, attr_list *attrs);
+
+CK_RV mech_synthesize(tpm_ctx *tctx,
+        CK_MECHANISM_PTR mech, attr_list *attrs,
+        CK_BYTE_PTR inbuf, CK_ULONG inlen,
+        CK_BYTE_PTR outbuf, CK_ULONG_PTR outlen);
+
+CK_RV mech_is_synthetic(tpm_ctx *tctx, CK_MECHANISM_PTR mech,
+        bool *is_synthetic);
+
+CK_RV mech_get_supported(tpm_ctx *tctx, CK_MECHANISM_TYPE_PTR mechlist, CK_ULONG_PTR count);
+
+CK_RV mech_is_hashing_needed(CK_MECHANISM_PTR mech,
+        bool *is_hashing_needed);
+
+CK_RV mech_get_digest_alg(CK_MECHANISM_PTR mech,
+        CK_MECHANISM_TYPE *mech_type);
+
+CK_RV mech_get_digester(CK_MECHANISM_PTR mech,
+        const EVP_MD **md);
+
+CK_RV mech_get_tpm_opdata(tpm_ctx *tctx, CK_MECHANISM_PTR mech, tobject *tobj, tpm_op_data **opdata);
+
+CK_RV mech_get_info(tpm_ctx *tctx, CK_MECHANISM_TYPE mech_type, CK_MECHANISM_INFO_PTR info);
+
+#endif /* SRC_LIB_MECH_H_ */

commit 2a1fec785f420c12a735161a8613cd3928eb2a4f

sign: fix hash_len type for 32 bit compilation
The wrong type causes errors for 32 bit builds of the form

src/lib/sign.c: In function 'sign_final_ex':
src/lib/sign.c:231:83: error: passing argument 4 of 'digest_final_op' from incompatible pointer type [-Werror=incompatible-pointer-types]
  231 |         rv = digest_final_op(ctx, opdata->digest_opdata, (CK_BYTE_PTR)digest_buf, &hash_len);
      |                                                                                   ^~~~~~~~~
      |                                                                                   |
      |                                                                                   size_t * {aka unsigned int *}
In file included from src/lib/sign.c:9:
src/lib/digest.h:35:107: note: expected 'CK_ULONG_PTR' {aka 'long unsigned int *'} but argument is of type 'size_t *' {aka 'unsigned int *'}
   35 | CK_RV digest_final_op(session_ctx *ctx, digest_op_data *supplied_opdata, CK_BYTE_PTR digest, CK_ULONG_PTR digest_len);
      |                                                                                              ~~~~~~~~~~~~~^~~~~~~~~~

Fixes: d80809ac3b8267f346f90bc3c73275bd8c66456e
Signed-off-by: Jonas Witschel <diabonas@gmx.de>

src/lib/sign.c
@@ -216,7 +216,7 @@ CK_RV sign_final_ex(session_ctx *ctx, CK_BYTE_PTR signature, CK_ULONG_PTR signat
 
     if (opdata->do_hash) {
 
-        size_t hash_len = utils_get_halg_size(opdata->mech.mechanism);
+        CK_ULONG hash_len = utils_get_halg_size(opdata->mech.mechanism);
         if (!hash_len) {
             LOGE("Hash algorithm cannot have 0 size");
             return CKR_GENERAL_ERROR;