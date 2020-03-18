d9b5f1a6b80986e37875dfae972184974633cabc
Andreas Fuchs <andreas.fuchs@sit.fraunhofer.de>
2020-03-18 16:01:34 +0100

    Tests: Add p11tool tests
    Add p11tool tests for esysdb as well as fapi backend.
    Skipping on p11tool < 3.5.0 due to segfault in binary.
    Disabling asan on fedora with clang due to infinite lock in binary.
    
    Signed-off-by: Andreas Fuchs <andreas.fuchs@sit.fraunhofer.de>

--- .ci/docker-prelude.sh
+++ .ci/docker-prelude.sh
@@ -15,6 +15,16 @@ get_deps "$WORKSPACE"
 export LD_LIBRARY_PATH=/usr/local/lib/
 export PATH=/root/.local/bin/:/ibmtpm974/src:$PATH
 
+# Unfortunately, p11tool unlearned the option for $HOME/.config/pkcs11/modules
+# This is true for Fedora 30 container and upstream
+# Thus we have to define it system-wide in this container
+if [ ! -e /etc/pkcs11/modules/tpm2_pkcs11.module ]; then
+  echo "Creating /etc/pkcs11/modules/tpm2_pkcs11.module"
+  mkdir -p /etc/pkcs11/modules || true
+  echo "module: $TRAVIS_BUILD_DIR/build/src/.libs/libtpm2_pkcs11.so" \
+       >/etc/pkcs11/modules/tpm2_pkcs11.module
+fi
+
 echo "echo changing to $TRAVIS_BUILD_DIR"
 # Change to the the travis build dir
 cd $TRAVIS_BUILD_DIR
