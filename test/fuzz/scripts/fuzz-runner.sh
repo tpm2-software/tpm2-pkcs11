c63e30e9cf8b624615addf39549e54f9f0ee609f
William Roberts <william.c.roberts@intel.com>
2020-04-28 13:42:01 -0500

    fuzz: fix comment in fuzz-runner
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/fuzz/scripts/fuzz-runner.sh
+++ test/fuzz/scripts/fuzz-runner.sh
@@ -6,7 +6,7 @@ set -x
 
 # see:
 #  - https://stackoverflow.com/questions/1215538/extract-parameters-before-last-parameter-in
-# for details on this. it moves the last argument (the fuzz-target) the front and the
+# for details on this. it moves the last argument (the fuzz-target) to the front and the
 # AM_FUZZ_LOG_FLAGS after the executable fuzz target...whew.
 env ${@:$#} ${*%${!#}}
 
