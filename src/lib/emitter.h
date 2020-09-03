9681849ff0bc132f34dfbd5ab09ad8626d86872c
William Roberts <william.c.roberts@intel.com>
2020-09-03 10:14:33 -0500

    test_db: add missing RV checks
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- src/lib/emitter.h
+++ src/lib/emitter.h
@@ -4,10 +4,11 @@
 #define SRC_LIB_EMITTER_H_
 
 #include "attrs.h"
+#include "debug.h"
 #include "pkcs11.h"
 #include "token.h"
 
-char *emit_attributes_to_string(attr_list *attrs);
+WEAK char *emit_attributes_to_string(attr_list *attrs);
 
 char *emit_config_to_string(token *tok);
 
