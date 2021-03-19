4fd935fcbd697c7698444dbd686dd6885a76569e
William Roberts <william.c.roberts@intel.com>
2021-03-19 13:56:51 -0500

    db: fix db update return code handling
    The rv value was being shadowed by a local scope variable and thus
    wasn't propogating to the caller. If a db update failed, it would have
    been ignored.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- src/lib/db.c
+++ src/lib/db.c
@@ -1793,7 +1793,7 @@ static CK_RV db_update(sqlite3 **xdb, const char *dbpath, unsigned old_version, 
      */
     size_t i;
     for(i=old_version; i < ARRAY_LEN(updaters) && i < new_version; i++) {
-        CK_RV rv = updaters[i](dbbak);
+        rv = updaters[i](dbbak);
         if (rv != CKR_OK) {
             LOGE("Running updater index %zu failed", i);
             goto out;
