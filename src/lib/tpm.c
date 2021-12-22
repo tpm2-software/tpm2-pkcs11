468aa7fca60b7342e55d328771948c97a8ffae83
张亚 <zhangya@uniontech.com>
2021-12-22 21:25:42 +0800

    fix: Esys_GetRandom memory leak
    Signed-off-by: 张亚 <zhangya@uniontech.com>

--- src/lib/tpm.c
+++ src/lib/tpm.c
@@ -793,6 +793,7 @@ bool tpm_getrandom(tpm_ctx *ctx, BYTE *data, size_t size) {
             &rand_bytes);
         if (rval != TSS2_RC_SUCCESS) {
             LOGE("Esys_GetRandom: %s:", Tss2_RC_Decode(rval));
+            Esys_Free(rand_bytes);
             goto out;
         }
 
@@ -800,12 +801,13 @@ bool tpm_getrandom(tpm_ctx *ctx, BYTE *data, size_t size) {
 
         offset += rand_bytes->size;
         size -= rand_bytes->size;
+        Esys_Free(rand_bytes);
+        rand_bytes = NULL;
     }
 
     result = true;
 
 out:
-    free(rand_bytes);
 
     return result;
 }
