f5944945407cf2f8c40414292cc2d0afd46b8ea7
William Roberts <william.c.roberts@intel.com>
2022-03-10 14:15:46 -0600

    ci: fixup dependency map for ossl3
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- .ci/download-deps.sh
+++ .ci/download-deps.sh
@@ -3,10 +3,24 @@
 
 function get_deps() {
 
+	source "${DOCKER_BUILD_DIR}/test/integration/scripts/helpers.sh"
+	check_openssl_version
+	if [ "$OSSL3_DETECTED" -eq "1" ]; then
+		engine_pkg="tpm2-openssl"
+		engine_flags=""
+		engine_version="master"
+		tpm2_tss_version="3.2.0"
+	else
+		engine_pkg="tpm2-tss-engine"
+		engine_flags="--enable-tctienvvar"
+		engine_version="v1.1.0"
+		tpm2_tss_version="3.0.0"
+        fi
+
 	# The list order is important and thus we can't use the keys of the dictionary as order is not preserved.
-	local github_deps=("tpm2-tss" "tpm2-abrmd" "tpm2-tools" "tpm2-tss-engine")
-	declare -A local config_flags=( ["tpm2-tss"]="--disable-doxygen-doc --enable-debug" ["tpm2-abrmd"]="--enable-debug" ["tpm2-tools"]="--disable-hardening --enable-debug" ["tpm2-tss-engine"]="--enable-tctienvvar")
-	declare -A local versions=( ["tpm2-tss"]="3.0.0" ["tpm2-abrmd"]="2.3.3" ["tpm2-tools"]="5.2-rc0" ["tpm2-tss-engine"]="v1.1.0")
+	local github_deps=("tpm2-tss" "tpm2-abrmd" "tpm2-tools" "${engine_pkg}")
+	declare -A local config_flags=( ["tpm2-tss"]="--disable-doxygen-doc --enable-debug" ["tpm2-abrmd"]="--enable-debug" ["tpm2-tools"]="--disable-hardening --enable-debug" ["${engine_pkg}"]="${engine_flags}")
+	declare -A local versions=( ["tpm2-tss"]="${tpm2_tss_version}" ["tpm2-abrmd"]="2.3.3" ["tpm2-tools"]="5.2" ["${engine_pkg}"]="${engine_version}")
 
 	echo "pwd starting: `pwd`"
 	pushd "$1"
