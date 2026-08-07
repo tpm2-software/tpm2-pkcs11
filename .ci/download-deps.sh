982acac3426c2fd52cf448d194593a0cbe19203c
Moritz Buhl <moritz.buhl@infineon.com>
2026-08-07 09:25:07 +0200

    ci: use the master branches of other tpm2-software projects
    The previously used versions would not work with newer CI container
    images as, e.g., OpenSSL removed engines with OpenSSL 3, cmocka removed
    depricated functions.
    
    Signed-off-by: Moritz Buhl <moritz.buhl@infineon.com>

--- .ci/download-deps.sh
+++ .ci/download-deps.sh
@@ -8,19 +8,15 @@ function get_deps() {
 	if [ "$OSSL3_DETECTED" -eq "1" ]; then
 		engine_pkg="tpm2-openssl"
 		engine_flags=""
-		engine_version="master"
-		tpm2_tss_version="3.2.0"
 	else
 		engine_pkg="tpm2-tss-engine"
 		engine_flags="--enable-tctienvvar"
-		engine_version="v1.1.0"
-		tpm2_tss_version="3.0.0"
-        fi
+	fi
 
 	# The list order is important and thus we can't use the keys of the dictionary as order is not preserved.
 	local github_deps=("tpm2-tss" "tpm2-abrmd" "tpm2-tools" "${engine_pkg}")
 	declare -A local config_flags=( ["tpm2-tss"]="--disable-doxygen-doc --enable-debug" ["tpm2-abrmd"]="--enable-debug" ["tpm2-tools"]="--disable-hardening --enable-debug" ["${engine_pkg}"]="${engine_flags}")
-	declare -A local versions=( ["tpm2-tss"]="${tpm2_tss_version}" ["tpm2-abrmd"]="2.3.3" ["tpm2-tools"]="5.2" ["${engine_pkg}"]="${engine_version}")
+	declare -A local versions=( ["tpm2-tss"]="master" ["tpm2-abrmd"]="master" ["tpm2-tools"]="master" ["${engine_pkg}"]="master")
 
 	echo "pwd starting: `pwd`"
 	pushd "$1"
@@ -34,7 +30,7 @@ function get_deps() {
 			continue
 		fi
 		v=${versions[$p]}
-		git clone --depth 1 --branch $v "https://github.com/tpm2-software/$p.git"
+		git clone --branch $v "https://github.com/tpm2-software/$p.git"
 
 		pushd "$p"
 
@@ -47,11 +43,24 @@ function get_deps() {
 
 	done;
 
-        # install tpm2-pytss package
+	# install Python helper packages. tpm2-pytss must match the tpm2-tss
+	# headers installed above, so build it from master instead of the stale
+	# PyPI wheel.
 	# older versions of clang cannot build the wheel, gcc is always present, use it.
 	OLD_CC="$CC"
 	CC=gcc
-	python3 -m pip install --break-system-packages pyasn1 pyasn1_modules python-pkcs11 tpm2-pytss
+	python3 -m pip install --break-system-packages pyasn1 pyasn1_modules python-pkcs11
+	if [ -d "tpm2-pytss" ]; then
+		echo "Skipping tpm2-pytss, already downloaded"
+	else
+		git clone --branch master "https://github.com/tpm2-software/tpm2-pytss.git"
+	fi
+	# Run ldconfig so tpm2-pytss can find the tss2 libs built above
+	ldconfig
+
+	pushd tpm2-pytss
+	python3 -m pip install --break-system-packages .
+	popd
 	CC="$OLD_CC"
 
 	# leave the download location directory
